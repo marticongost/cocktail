83fbbfebb993886a36525f6e5ccb4b3dd27db472
Mart? Congost <marti.congost@whads.com>
2008-11-25 10:45:34 +0100

    Added test cases for bidirectional relations

---
+++ tests/persistence/__init__.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+"""
+Test suite for the 'persistence' package of the Cocktail web development
+toolkit.
+
+@author:		Martí Congost
+@contact:		marti.congost@whads.com
+@organization:	Whads/Accent SL
+@since:			November 2008
+"""
+from cocktail.tests.persistence.relationtests import *
+
+if __name__ == "__main__":
+    from unittest import main
+    main()
+
