381631ed30f2061e431db3c347a62851fd737c92
Mart? Congost <marti.congost@whads.com>
2008-12-05 13:33:53 +0100

    Fixed data binding in html.TextArea, make it inherit from DataBoundControl

--- html/textarea.py
+++ html/textarea.py
@@ -6,8 +6,27 @@
 @organization:	Whads/Accent SL
 @since:			September 2008
 """
-from cocktail.html import Element
+from cocktail.html import Element, Content
+from cocktail.html.databoundcontrol import DataBoundControl
 
-class TextArea(Element):
+
+class TextArea(Element, DataBoundControl):
     tag = "textarea"
+
+    def __init__(self, *args, **kwargs):
+        Element.__init__(self, *args, **kwargs)
+        DataBoundControl.__init__(self)
+        self.__content = Content()
+        self.append(self.__content)
+
+    def _get_value(self):
+        return self.__content.value
+    
+    def _set_value(self, value):
+        self.__content.value = value
+
+    value = property(_get_value, _set_value, doc = """
+        Gets or sets the text area's value.
+        @type: str
+        """)
 
