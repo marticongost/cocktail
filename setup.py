5ff4fae813c8b4386f9e33c4307eb783ac0dc500
Mart? Congost <marti.congost@whads.com>
2008-12-09 13:27:18 +0100

    Integration with setuptools and buffet

---
+++ setup.py
@@ -0,0 +1,32 @@
+#-*- coding: utf-8 -*-
+"""
+
+@author:		Martí Congost
+@contact:		marti.congost@whads.com
+@organization:	Whads/Accent SL
+@since:			December 2008
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name = "cocktail",
+    version = "0.1a",
+    author = "Whads/Accent SL",
+    author_email = "tech@whads.com",
+    description = """A tasty mix of python web development utilities.""",
+    install_requires = ["simplejson", "ZODB3==3.8", "cherrypy>=3"],
+    include_package_data = True,    
+    packages = find_packages(),
+
+    # Cocktail can't yet access view resources (images, style sheets, client
+    # side scripts, etc) that are packed inside a zipped egg, so distribution
+    # in zipped form is disabled
+    zip_safe = False,
+
+    # Make CML templates available to Buffet
+    entry_points = {
+        "python.templating.engines":
+        ["cocktail=cocktail.html.templates.buffetplugin:CocktailBuffetPlugin"]
+    }
+)
+
