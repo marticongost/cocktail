b0e77d91698f18c17aebaf119e99807374d9ff09
Mart? Congost <marti.congost@whads.com>
2008-12-19 13:45:22 +0100

    Improvements to html.TreeView * Items in the tree can now be filtered (override the filter_item() method) * Items can be converted into links (override the get_item_url() method)

--- cocktail/html/treeview.py
+++ cocktail/html/treeview.py
@@ -9,6 +9,10 @@
 from cocktail.translations import translate
 from cocktail.html import Element
 
+NOT_ACCESSIBLE = 0
+ACCESSIBLE = 1
+ACCESSIBLE_DESCENDANTS = 2
+
 
 class TreeView(Element):
 
@@ -16,18 +20,48 @@ class TreeView(Element):
     root = None
     root_visible = True
     create_empty_containers = False
+    filter_item = None
+    __item_access = None
+
+    def _is_accessible(self, item):
+        
+        if self.__item_access is None:
+            self.__item_access = {}
+        
+        accessibility = self.__item_access.get(item)
+
+        if accessibility is None:
+
+            if self.filter_item(item):
+                accessibility = ACCESSIBLE
+            elif any(
+                self._is_accessible(child)
+                for child in self.get_child_items(item)
+            ):
+                accessibility = ACCESSIBLE_DESCENDANTS
+            else:
+                accessibility = NOT_ACCESSIBLE
+
+            self.__item_access[item] = accessibility
+
+        return accessibility
 
     def _ready(self):
+        Element._ready(self)
+        
         if self.root is not None:
             if self.root_visible:
-                self.root_entry = self.create_entry(self.root)
-                self.append(self.root_entry)
+                if not self.filter_item or self._is_accessible(self.root):
+                    self.root_entry = self.create_entry(self.root)
+                    self.append(self.root_entry)
             else:
                 self._fill_children_container(
                     self,
                     self.root,
                     self.get_child_items(self.root)
-                )                
+                )
+
+        self.__item_access = None
 
     def create_entry(self, item):
         
@@ -48,6 +82,16 @@ class TreeView(Element):
         label = Element("div")
         label.add_class("entry_label")
         label.append(self.get_item_label(item))
+
+        if self.filter_item \
+        and self._is_accessible(item) != ACCESSIBLE:
+            label.add_class("filtered")
+        else:
+            url = self.get_item_url(item)
+            if url is not None:
+                label.tag = "a"
+                label["href"] = url
+
         return label
 
     def get_item_label(self, item):
@@ -61,8 +105,12 @@ class TreeView(Element):
     def _fill_children_container(self, container, item, children):
         if children:
             for child in children:
-                container.append(self.create_entry(child))
+                if not self.filter_item or self._is_accessible(child):
+                    container.append(self.create_entry(child))
 
     def get_child_items(self, parent):
         return parent.children
+        
+    def get_item_url(self, content_type):
+        return None
 
