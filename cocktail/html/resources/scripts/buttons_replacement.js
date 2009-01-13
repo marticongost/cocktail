1377a261c37eb3d11395b0946006e465d1caeba8
C??sar Ebrat <cesar.ebrat@whads.com>
2009-01-13 13:01:40 +0100

    added blur action in datetime input box to fill it with 0 and modified buttons action

--- cocktail/html/resources/scripts/buttons_replacement.js
+++ cocktail/html/resources/scripts/buttons_replacement.js
@@ -2,21 +2,19 @@ jQuery(document).ready( function () {
     
     var button_names = [];
     
-    jQuery(".Form button").each( function () {
-        var name = jQuery(this).attr('name');
+    jQuery("form button").each( function () {
+        var name = jQuery(this).attr('name');        
         if(button_names.indexOf(name) < 0) {
-            var replaced = document.createElement('input');
-            var class_name =  'replaced-' + jQuery(this).attr('id');           
-            replaced.className = class_name;        
+            var replaced = document.createElement('input');       
             jQuery(replaced).attr({
                 type: "hidden",
                 name: name
             }).insertBefore(jQuery(this));             
             button_names.push(name);
         }
-        jQuery(this).removeAttr('name');
-        jQuery(this).click( function () {
-            jQuery(replaced).val(jQuery(this).val());
+        jQuery(this).removeAttr('name');        
+        jQuery(this).click( function () {          
+            jQuery("input[name='" + name + "']").val(jQuery(this).val());
         });
     });
     
