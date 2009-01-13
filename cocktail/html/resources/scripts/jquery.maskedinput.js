dc42bcfd7f4b1f6ed995a5eefd5fecbbeae048a7
César Ebrat <cebrat@gmail.com>
2009-01-13 13:01:40 +0100

    added blur action in datetime input box to fill it with 0 and modified buttons action

--- cocktail/html/resources/scripts/jquery.maskedinput.js
+++ cocktail/html/resources/scripts/jquery.maskedinput.js
@@ -1,6 +1,275 @@
+﻿/// <reference path="../../../lib/jquery-1.2.6.js" />
 /*
- * Masked Input Plugin for jQuery 1.2.1
- * Copyright (c) 2008 Josh Bush (digitalbush.com)
- * Licensed under the MIT (MIT-LICENSE.txt) 
- */
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(4($){2 w=($.21.1V?\'1U\':\'1Q\')+".C";2 x=(1o.1E!=1A);$.C={1m:{\'9\':"[0-9]",\'a\':"[A-1k-z]",\'*\':"[A-1k-1s-9]"}};$.28.1i({D:4(b,c){3(5.y==0)6;3(1h b==\'1f\'){c=(1h c==\'1f\')?c:b;6 5.11(4(){3(5.13){5.1e();5.13(b,c)}B 3(5.1d){2 a=5.1d();a.1B(V);a.1z(\'Y\',c);a.1c(\'Y\',b);a.1x()}})}B{3(5[0].13){b=5[0].1w;c=5[0].1v}B 3(15.S&&15.S.1a){2 d=15.S.1a();b=0-d.1D().1c(\'Y\',-1y);c=b+d.29.y}6{I:b,W:c}}},X:4(){6 5.1F("X")},C:4(m,n){3(!m&&5.y>0){2 o=$(5[0]);2 q=o.R("12");6 $.18(o.R("14"),4(c,i){6 q[i]?c:E}).19(\'\')}n=$.1i({F:"1G",U:E},n);2 r=$.C.1m;2 q=[];2 s=m.y;2 u=E;2 v=m.y;$.11(m.1b(""),4(i,c){3(c==\'?\'){v--;s=i}B{q.1I(r[c]?20 22(r[c]):E);3(q[q.y-1]&&u==E)u=q.y-1}});6 5.11(4(){2 f=$(5);2 g=$.18(m.1b(""),4(c,i){3(c!=\'?\')6 r[c]?n.F:c});2 h=G;2 l=f.7();f.R("14",g).R("12",q);4 K(a){Z(++a<v){3(q[a])6 a}6 v};4 1g(a){Z(!q[a]&&a>=0)a--;P(2 i=a;i<v;i++){3(q[i]){g[i]=n.F;2 j=K(i);3(j<v&&q[i].O(g[j])){g[i]=g[j]}B Q}}H();f.D(1t.1u(u,a))};4 1j(a){P(2 i=a,c=n.F;i<v;i++){3(q[i]){2 j=K(i);2 t=g[i];g[i]=c;3(j<v&&q[j].O(t))c=t;B Q}}};4 1l(e){2 a=$(5).D();2 k=e.10;h=(k<16||(k>16&&k<17)||(k>17&&k<1n));3((a.I-a.W)!=0&&(!h||k==8||k==T))M(a.I,a.W);3(k==8||k==T||(x&&k==1H)){1g(a.I+(k==T?0:-1));6 G}B 3(k==27){M(0,v);H();$(5).D(u);6 G}};4 1p(e){3(h){h=G;6(e.10==8)?G:E}e=e||1o.1J;2 k=e.1K||e.10||e.1L;2 a=$(5).D();3(e.1M||e.1N){6 V}B 3((k>=1n&&k<=1O)||k==17||k>1P){2 p=K(a.I-1);3(p<v){2 c=1R.1S(k);3(q[p].O(c)){1j(p);g[p]=c;H();2 b=K(p);$(5).D(b);3(n.U&&b==v)n.U.1T(f)}}}6 G};4 M(a,b){P(2 i=a;i<b&&i<v;i++){3(q[i])g[i]=n.F}};4 H(){6 f.7(g.19(\'\')).7()};4 J(a){2 b=f.7();2 d=-1;P(2 i=0,N=0;i<v;i++){3(q[i]){g[i]=n.F;Z(N++<b.y){2 c=b.1W(N-1);3(q[i].O(c)){g[i]=c;d=i;Q}}3(N>b.y)Q}}3(!a&&d+1<s){f.7("");M(0,v)}B 3(a||d+1>=s){H();3(!a)f.7(f.7().1X(0,d+1))}6(s?i:u)};f.1Y("X",4(){f.1Z(".C").1q("14").1q("12")}).L("1e.C",4(){l=f.7();2 a=J();H();1r(4(){f.D(a)},0)}).L("23.C",4(){J();3(f.7()!=l)f.24()}).L("25.C",1l).L("26.C",1p).L(w,4(){1r(4(){f.D(J(V))},0)});J()})}})})(1C);',62,134,'||var|if|function|this|return|val|||||||||||||||||||||||||||length|||else|mask|caret|null|placeholder|false|writeBuffer|begin|checkVal|seekNext|bind|clearBuffer|pos|test|for|break|data|selection|46|completed|true|end|unmask|character|while|keyCode|each|tests|setSelectionRange|buffer|document||32|map|join|createRange|split|moveStart|createTextRange|focus|number|shiftL|typeof|extend|shiftR|Za|keydownEvent|definitions|41|window|keypressEvent|removeData|setTimeout|z0|Math|max|selectionEnd|selectionStart|select|100000|moveEnd|undefined|collapse|jQuery|duplicate|orientation|trigger|_|127|push|event|charCode|which|ctrlKey|altKey|122|186|input|String|fromCharCode|call|paste|msie|charAt|substring|one|unbind|new|browser|RegExp|blur|change|keydown|keypress||fn|text'.split('|'),0,{}))
+* Copyright (c) 2007-2008 Josh Bush (digitalbush.com)
+* 
+* Permission is hereby granted, free of charge, to any person
+* obtaining a copy of this software and associated documentation
+* files (the "Software"), to deal in the Software without
+* restriction, including without limitation the rights to use,
+* copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following
+* conditions:
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE. 
+*/
+
+/*
+* Version: 1.2.1
+* Release: 2008-12-21
+*/
+(function($) {
+	var pasteEventName = ($.browser.msie ? 'paste' : 'input') + ".mask";
+	var iPhone = (window.orientation!=undefined); 
+
+	$.mask = {
+		//Predefined character definitions
+		definitions: {
+			'9': "[0-9]",
+			'a': "[A-Za-z]",
+			'*': "[A-Za-z0-9]"
+		}
+	};
+
+	$.fn.extend({
+		//Helper Function for Caret positioning
+		caret: function(begin, end) {
+			if (this.length == 0) return;
+			if (typeof begin == 'number') {
+				end = (typeof end == 'number') ? end : begin;
+				return this.each(function() {
+					if (this.setSelectionRange) {
+						this.focus();
+						this.setSelectionRange(begin, end);
+					} else if (this.createTextRange) {
+						var range = this.createTextRange();
+						range.collapse(true);
+						range.moveEnd('character', end);
+						range.moveStart('character', begin);
+						range.select();
+					}
+				});
+			} else {
+				if (this[0].setSelectionRange) {
+					begin = this[0].selectionStart;
+					end = this[0].selectionEnd;
+				} else if (document.selection && document.selection.createRange) {
+					var range = document.selection.createRange();
+					begin = 0 - range.duplicate().moveStart('character', -100000);
+					end = begin + range.text.length;
+				}
+				return { begin: begin, end: end };
+			}
+		},
+		unmask: function() { return this.trigger("unmask"); },
+		mask: function(mask, settings) {
+			if (!mask && this.length > 0) {
+				var input = $(this[0]);
+				var tests = input.data("tests");
+				return $.map(input.data("buffer"), function(c, i) {
+					return tests[i] ? c : null;
+				}).join('');
+			}
+			settings = $.extend({
+				placeholder: "_",
+				completed: null,
+				fullfilled: false,
+				maskedtype: ""
+			}, settings);
+
+			var defs = $.mask.definitions;
+			var tests = [];
+			var partialPosition = mask.length;
+			var firstNonMaskPos = null;
+			var len = mask.length;
+
+			$.each(mask.split(""), function(i, c) {
+				if (c == '?') {
+					len--;
+					partialPosition = i;
+				} else {
+					tests.push(defs[c] ? new RegExp(defs[c]) : null);
+					if (tests[tests.length - 1] && firstNonMaskPos == null)
+						firstNonMaskPos = tests.length - 1;
+				}
+			});
+
+			return this.each(function() {
+				var input = $(this);
+				var buffer = $.map(mask.split(""), function(c, i) { if (c != '?') return defs[c] ? settings.placeholder : c });
+				var ignore = false;  			//Variable for ignoring control keys
+				var focusText = input.val();
+
+				input.data("buffer", buffer).data("tests", tests);
+
+				function seekNext(pos) {
+					while (++pos < len) {
+						if (tests[pos])
+							return pos;
+					}
+					return len;
+				};
+
+				function shiftL(pos) {
+					while (!tests[pos] && pos >= 0) pos--;
+					for (var i = pos; i < len; i++) {
+						if (tests[i]) {
+							buffer[i] = settings.placeholder;
+							var j = seekNext(i);
+							if (j < len && tests[i].test(buffer[j])) {
+								buffer[i] = buffer[j];
+							} else
+								break;
+						}
+					}
+					writeBuffer();
+					input.caret(Math.max(firstNonMaskPos, pos));
+				};
+
+				function shiftR(pos) {
+					for (var i = pos, c = settings.placeholder; i < len; i++) {
+						if (tests[i]) {
+							var j = seekNext(i);
+							var t = buffer[i];
+							buffer[i] = c;
+							if (j < len && tests[j].test(t)) 
+								c = t;
+							else
+								break;
+						}
+					}
+				};
+
+				function keydownEvent(e) {
+					var pos = $(this).caret();
+					var k = e.keyCode;
+					ignore = (k < 16 || (k > 16 && k < 32) || (k > 32 && k < 41));
+
+					//delete selection before proceeding
+					if ((pos.begin - pos.end) != 0 && (!ignore || k == 8 || k == 46)) 
+						clearBuffer(pos.begin, pos.end);
+					
+					//backspace, delete, and escape get special treatment
+					if (k == 8 || k == 46 || (iPhone && k==127)) {//backspace/delete
+						shiftL(pos.begin + (k == 46 ? 0 : -1));
+						return false;
+					} else if (k == 27) {//escape
+						clearBuffer(0, len);
+						writeBuffer();
+						$(this).caret(firstNonMaskPos);
+						return false;
+					}
+				};
+
+				function keypressEvent(e) {
+					if (ignore) {
+						ignore = false;
+						//Fixes Mac FF bug on backspace
+						return (e.keyCode == 8) ? false : null;
+					}
+					e = e || window.event;
+					var k = e.charCode || e.keyCode || e.which;
+					var pos = $(this).caret();
+
+					if (e.ctrlKey || e.altKey) {//Ignore
+						return true;
+					} else if ((k >= 41 && k <= 122) || k == 32 || k > 186) {//typeable characters
+						var p = seekNext(pos.begin - 1);
+						if (p < len) {
+							var c = String.fromCharCode(k);
+							if (tests[p].test(c)) {
+								shiftR(p);
+								buffer[p] = c;
+								writeBuffer();
+								var next = seekNext(p);
+								$(this).caret(next);
+								if (settings.completed && next == len)
+									settings.completed.call(input);
+							}
+						}
+					}
+					return false;
+				};
+
+				function clearBuffer(start, end) {
+					for (var i = start; i < end && i < len; i++) {
+						if (tests[i])
+							buffer[i] = settings.placeholder;
+					}
+				};
+
+				function writeBuffer() { return input.val(buffer.join('')).val(); };
+
+				function checkVal(allow) {
+					//try to place characters where they belong
+					var test = input.val();					
+					var lastMatch = -1;
+					for (var i = 0,pos=0; i < len; i++) {
+						if (tests[i]) {
+							buffer[i] = settings.placeholder;
+							while (pos++ < test.length) {
+								var c = test.charAt(pos - 1);
+								if (tests[i].test(c)) {
+									buffer[i] = c;
+									lastMatch = i;
+									break;
+								}
+							}
+							if (pos > test.length)
+								break;
+						}
+					}
+					if (!allow && lastMatch + 1 < partialPosition) {
+					    if(settings.fullfilled && settings.maskedtype == "time"){							    
+							input.val(input.val().replace(/_/g,"0"));
+					    }else{
+						    input.val("");
+						}
+						clearBuffer(0, len);
+					} else if (allow || lastMatch + 1 >= partialPosition) {
+						writeBuffer();
+						if(!allow)input.val(input.val().substring(0, lastMatch + 1));
+					}					
+					return (partialPosition ? i : firstNonMaskPos);
+				};
+
+				input
+					.one("unmask", function() {
+						input
+							.unbind(".mask")
+							.removeData("buffer")
+							.removeData("tests");
+					})
+					.bind("focus.mask", function() {
+						focusText = input.val();
+						var pos = checkVal();
+						writeBuffer();
+						setTimeout(function() {
+							input.caret(pos);
+						}, 0);
+					})
+					.bind("blur.mask", function() {
+						checkVal();												
+						if (input.val() != focusText)
+                                input.change();
+					})
+					.bind("keydown.mask", keydownEvent)
+					.bind("keypress.mask", keypressEvent)
+					.bind(pasteEventName, function(){ 
+						setTimeout(function(){input.caret(checkVal(true));},0);
+					});
+
+				checkVal(); //Perform initial check for existing values
+			});
+		}
+	});
+})(jQuery);
