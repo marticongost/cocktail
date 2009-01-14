b502162c8b5c538f1172c1e9970922fa0bde9278
César Ebrat <cebrat@gmail.com>
2009-01-14 10:28:53 +0100

    added type="submit" to all buttons tag (bugfix ie)

--- cocktail/html/resources/scripts/buttons_replacement.js
+++ cocktail/html/resources/scripts/buttons_replacement.js
@@ -1,21 +1,62 @@
 jQuery(document).ready( function () {
     
+    if(!Array.indexOf){
+	    Array.prototype.indexOf = function(obj){
+	        for(var i=0; i<this.length; i++){
+	            if(this[i]==obj){
+	                return i;
+	            }
+	        }
+	        return -1;
+	    }
+	}
+
+    
     var button_names = [];
     
-    jQuery("form button").each( function () {
-        var name = jQuery(this).attr('name');        
-        if(button_names.indexOf(name) < 0) {
-            var replaced = document.createElement('input');       
-            jQuery(replaced).attr({
-                type: "hidden",
-                name: name
-            }).insertBefore(jQuery(this));             
-            button_names.push(name);
+    jQuery("form button").each( function (index) {
+        
+        var nom, replaced
+        
+        if(jQuery(this).attr('name') == ""){
+            nom = "joker_" + index;
+        }else{
+            nom = jQuery(this).attr('name');
+            jQuery(this).removeAttr('name');
         }
-        jQuery(this).removeAttr('name');        
-        jQuery(this).click( function () {          
-            jQuery("input[name='" + name + "']").val(jQuery(this).val());
-        });
+        
+        if(button_names.indexOf(nom) < 0) {
+            //TODO: Retirar con el control que carga el script solo en IE.
+            if(jQuery.browser.msie){                
+                replaced = document.createElement("<input type='hidden' name='" + nom + "'>");
+            }else{
+                replaced = document.createElement('input');
+                jQuery(replaced).attr({
+                    type: "hidden",
+                    name: nom
+                });
+            }            
+            jQuery(replaced).insertBefore(jQuery(this));             
+            button_names.push(nom);
+        }
+        /*            
+        this.setAttribute('type','submit');                              
+        var newInput = document.createElement('input');
+        newInput.type = 'submit'; // that should work even with IE
+        if(this.attributes.getNamedItem("value")){
+            var attr = document.createAttribute("value");
+            newInput.attributes.setNamedItem(attr);
+        }
+        //newInput.innerHTML = this.innerHTML;
+        this.parentNode.replaceChild(newInput, this);
+        */
+        if(this.attributes.getNamedItem("value")){
+            jQuery(this).click( function () {                                
+                jQuery("input[name='" + nom + "']").val(this.attributes.getNamedItem("value").nodeValue);                     
+            });
+        }
+        
     });
+    
     
 });
