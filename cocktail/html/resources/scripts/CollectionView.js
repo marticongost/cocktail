6f05deb7afd046b4af4ece3a86daa57c86f65cf7
Mart? Congost <marti.congost@whads.com>
2009-01-26 15:36:02 +0100

    UI improvements to html.CollectionView

--- cocktail/html/resources/scripts/CollectionView.js
+++ cocktail/html/resources/scripts/CollectionView.js
@@ -1,98 +1,95 @@
 jQuery(function () {
-     
-     var target = null;      
-     
-     jQuery("*", document.body).each( function() {               
+
+     var target = null;
+
+     jQuery("*", document.body).each( function() {
          //Dbl click target selector
          if (this.dblclick_target) target = this.dblclick_target;
       });
-    
+
     var target = target ? target : ".toolbar_button[value=edit]";
-    
+
     //Switch selection type
     //TODO:
-    
+
     var currentFocus = null;
-    jQuery('input, textarea').focus( function() {        
+    jQuery('input, textarea').focus( function() {
         currentFocus = this;
     }).blur( function() {
         currentFocus = null;
-    });  
-    
-    var checkboxes = jQuery(".Table .selection input");        
+    });
+
+    var checkboxes = jQuery(".Table .selection input");
     var lastSelected;
-    
-    jQuery(document).keydown(function (e) {                                        
-        
+
+    jQuery(document).keydown(function (e) {
+
         if(!currentFocus){
-           
+
             if(!lastSelected){
-                lastSelected = checkboxes[0];    
+                lastSelected = checkboxes[0];
             }
-            
-            var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;            
-            var selindex = checkboxes.index(lastSelected);        
-             
+
+            var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
+            var selindex = checkboxes.index(lastSelected);
+
     		switch(key) {
     		    case 13:
     		         if(jQuery(".Table .selection input:checked").length > 0) jQuery(target).click();
                   break;
     		    case 36:
-    		        removeSelection();		        
+    		        removeSelection();
                     if ( e.shiftKey ){
                         var lastIndex = checkboxes.index(checkboxes[0]);
-                        var selIndex = checkboxes.index(lastSelected);                  
+                        var selIndex = checkboxes.index(lastSelected);
                         var end = Math.max(selIndex,lastIndex);
                         var start = Math.min(selIndex,lastIndex);
-    
+
                         for(i=start;i<=end;i++) {
-                            checkboxes[i].checked = true;
-                            highlightSelection.call(checkboxes[i]);
+                            setChecked(checkboxes[i], true);
                         }
                     }else{
-                        lastSelected = checkboxes[0];                     			        
-    			        lastSelected.checked = true;
-                        highlightSelection.call(checkboxes[0]);
+    			        setChecked(checkboxes[0], true);
                     }
                   break;
     		    case 35:
                     removeSelection();
                     if ( e.shiftKey ){
                         var lastIndex = checkboxes.index(checkboxes[checkboxes.length-1]);
-                        var selIndex = checkboxes.index(lastSelected);                  
+                        var selIndex = checkboxes.index(lastSelected);
                         var end = Math.max(selIndex,lastIndex);
                         var start = Math.min(selIndex,lastIndex);
-    
+
                         for(i=start;i<=end;i++) {
-                            checkboxes[i].checked = true;
-                            highlightSelection.call(checkboxes[i]);
+                            setChecked(checkboxes[i], true);
                         }
                     }else{
         			    lastSelected = checkboxes[checkboxes.length-1];
-        			    lastSelected.checked = true;
-                        highlightSelection.call(checkboxes[checkboxes.length-1]);
+        			    setChecked(lastSelected, true);
                     }
                   break;
-    			case 38: 			  
-    			  if(selindex > 0){		  
+    			case 38:
+    			  if(selindex > 0){
     			    if ( !e.shiftKey ) removeSelection();
-    			    lastSelected = checkboxes[selindex-1];
-    			    lastSelected.checked = true;
-                    highlightSelection.call(checkboxes[selindex-1]);			      
+    			    setChecked(checkboxes[selindex-1], true);
     			  };
     			  break;
-    			case 40: 
+    			case 40:
     			  if(checkboxes.length > selindex+1) {
     			    if ( !e.shiftKey ) removeSelection();
-    			    lastSelected = checkboxes[selindex+1];
-    			    lastSelected.checked = true;
-                    highlightSelection.call(checkboxes[selindex+1]);
+    			    setChecked(checkboxes[selindex+1], true);                    
     			  }
-    			  break;			     
+    			  break;
     		}
     	}
     });
-      
+
+    function setChecked(control, checked) {        
+        control.checked = checked;
+        highlightSelection.call(control);
+        jQuery(control).trigger("selectionChanged");
+    }
+
     function highlightSelection() {
         var row = jQuery(this).parents("tr");
         if (this.checked) {
@@ -105,37 +102,34 @@ jQuery(function () {
 
     // Hide checkboxes, but keep them around for form submission purposes
     jQuery(".Table .selection").css({display: "none"});
-    
+
     checkboxes
         .each(highlightSelection)
-        .change(highlightSelection)
-        
+        .change(function () { setChecked(this, this.checked); });
+
     function removeSelection() {
-        jQuery(".Table .selection input:checked")
-                        .each(function () {                       
-                            this.checked = !this.checked;
-                            highlightSelection.call(this);                    
-        });   
+        jQuery(".Table .selection input:checked").each(function () {
+            setChecked(this, !this.checked);
+        });
     }
-    
+
     function selectAll() {
         checkboxes.each( function () {
-            this.checked = true;
-            highlightSelection.call(this);
-        });    
+            setChecked(this, true);
+        });
     }
 
     jQuery(".Table tbody tr")
-        
+
         // Togle row selection when clicking a row
-        .click(function (e) {                                            
-            
+        .click(function (e) {
+
             var src = e.target || e.srcElement;
-            var srcTag = src.tagName.toLowerCase();         
-          
+            var srcTag = src.tagName.toLowerCase();
+
             if (srcTag != "a" && srcTag != "input" && srcTag != "button" && srcTag != "textarea") {
-                
-                if ( e.shiftKey ) {                    	                   
+
+                if ( e.shiftKey ) {
                     var selIndex = checkboxes.index(jQuery(this).find(".selection input").get(0));
                     var lastIndex = checkboxes.index(lastSelected);
                     /*
@@ -147,35 +141,29 @@ jQuery(function () {
                     if ( selIndex == lastIndex ) {
                         return true;
                     }
-        
+
                     var end = Math.max(selIndex,lastIndex);
                     var start = Math.min(selIndex,lastIndex);
 
                     for(i=start;i<=end;i++) {
-                        checkboxes[i].checked = checkValue;
-                        highlightSelection.call(checkboxes[i]);
+                        setChecked(checkboxes[i], checkValue);
                     }
-                   
-                }else{
-                    
-                                       
-                    if ( !e.ctrlKey ) removeSelection();
-                                  
-                    lastSelected = jQuery(this).find(".selection input").get(0);                    
-                    lastSelected.checked = !lastSelected.checked;                                                   
-                    highlightSelection.call(lastSelected);
-                                                         
                 }
-                
+                else {
+                    if ( !e.ctrlKey ) removeSelection();
+
+                    lastSelected = jQuery(this).find(".selection input").get(0);
+                    setChecked(lastSelected, !lastSelected.checked);
+                }
+
                 if(srcTag == "label") e.preventDefault();
-            }                                                        
+            }
         })
 
-        .dblclick(function () {           
-        
+        .dblclick(function () {
+
             jQuery(".Table .selection input").each(function () {
-                this.checked = false;
-                highlightSelection.call(this);
+                setChecked(this, false);
             });
 
             if (jQuery(this).is(".Table tbody tr")) {
@@ -186,14 +174,12 @@ jQuery(function () {
             }
 
             jQuery(row).find(".selection input").each(function () {
-                this.checked = true;
-                highlightSelection.call(this);
+                setChecked(this, true);
             });
-            
+
             jQuery(target).click();
         });
-        
-        
+
         var texts = [
                 {
                     text: cocktail.translate("CollectionView selection"),
@@ -208,16 +194,16 @@ jQuery(function () {
                     cls: 'row_selector_none'
                 }
         ];
-       
+
     if(jQuery(".no_results").length==0){
-    
+
         var div = document.createElement('div');
         div.className = 'row_selector';
-        
-        for(i=0;i<texts.length;i++){            
+
+        for(i=0;i<texts.length;i++){
             var span = document.createElement('span');
-            
-            if(texts[i].cls!=""){                 
+
+            if(texts[i].cls!=""){
                 var link = document.createElement('a');
                 link.className = texts[i].cls;
                 link.appendChild(document.createTextNode(texts[i].text));
@@ -227,22 +213,26 @@ jQuery(function () {
             }
             div.appendChild(span);
         }
-        
-        jQuery(".toolbar").after(div);
-        
+
+        var rowSelectorPrev = jQuery(".search_results_message");
+        if (!rowSelectorPrev.length) {
+            rowSelectorPrev = jQuery(".toolbar");
+        }
+        rowSelectorPrev.after(div);
+
         jQuery(".row_selector_all").click( function () {
             selectAll();
         });
-        
+
         jQuery(".row_selector_none").click( function () {
             removeSelection();
         });
     }
-    
+
     function disableTextSelection () {
-        
+
         var selector = jQuery(".Table");
-        
+
         if (jQuery.browser.mozilla) {
             return selector.each(function() {
                 jQuery(this).css({
@@ -262,28 +252,7 @@ jQuery(function () {
                 });
             });
         }
-    } 
-    
-    disableTextSelection();
-
-    var EXPANSION_COOKIE = "CollectionView filters expanded";
-
-    // Make the filter box collapsible
-    jQuery(".CollectionView .filters")
-        .addClass("scripted")
-        .children(".label").click(function () {
-            var filters = jQuery(this).parents(".filters");
-            if (filters.hasClass("expanded")) {
-                filters.removeClass("expanded");
-                jQuery.cookie(EXPANSION_COOKIE, "collapsed");                
-            }
-            else {
-                filters.addClass("expanded");
-                jQuery.cookie(EXPANSION_COOKIE, "expanded");
-            }
-        });
-
-    if (jQuery.cookie(EXPANSION_COOKIE) == "expanded") {
-        jQuery(".CollectionView .filters:not(.empty) > .label").click();
     }
+
+    disableTextSelection();
 });
