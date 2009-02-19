bf8ff79df2ee815498deb38019fa8eb3cb8ef204
Mart? Congost <marti.congost@whads.com>
2009-02-19 18:43:03 +0100

    Change id generation to acquire identifiers in batches, to reduce the impact on the ZODB

--- cocktail/persistence/incremental_id.py
+++ cocktail/persistence/incremental_id.py
@@ -15,7 +15,9 @@ from ZODB.POSException import ConflictError
 
 ID_CONTAINER_KEY = "id_container"
 RETRY_INTERVAL = 0.1
+STEP = 10
 
+_acquired_ids = []
 _lock = Lock()
 
 @datastore.connection_opened.append
@@ -27,35 +29,43 @@ def create_container(event):
 
 def incremental_id(key = "default"):
     
+    global _acquired_ids
     _lock.acquire()
 
     try:
-        try:
-            tm = transaction.TransactionManager()
-            conn = datastore.db.open(transaction_manager = tm)
-            
-            while True:
-                conn.sync()
-                root = conn.root()
-                container = root.get(ID_CONTAINER_KEY)
-
-                if container is None:
-                    container = PersistentMapping()
-                    root[ID_CONTAINER_KEY] = container
+        if not _acquired_ids:            
+            try:
+                tm = transaction.TransactionManager()
+                conn = datastore.db.open(transaction_manager = tm)
                 
-                id = container.get(key, 0) + 1
-                container[key] = id
-                try:
-                    tm.commit()
-                except ConflictError:
-                    sleep(RETRY_INTERVAL)
-                    tm.abort()
-                except:
-                    tm.abort()
-                else:
-                    return id
-        finally:
-            conn.close()
+                while True:
+                    conn.sync()
+                    root = conn.root()
+                    container = root.get(ID_CONTAINER_KEY)
+
+                    if container is None:
+                        container = PersistentMapping()
+                        root[ID_CONTAINER_KEY] = container
+                    
+                    base_id = container.get(key, 0)
+                    top_id = base_id + STEP
+
+                    container[key] = top_id
+                    try:
+                        tm.commit()
+                    except ConflictError:
+                        sleep(RETRY_INTERVAL)
+                        tm.abort()
+                    except:
+                        tm.abort()
+                    else:
+                        _acquired_ids = range(base_id + 1, top_id + 1)
+                        break
+            finally:
+                conn.close()
+    
+        return _acquired_ids.pop(0)
+    
     finally:
         _lock.release()
 
