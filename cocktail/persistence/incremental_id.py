6bae2829555465521f711176ca8d36ba9795cfaa
Mart? Congost <marti.congost@whads.com>
2009-02-19 10:10:47 +0100

    Minimize conflict errors caused by the new incremental_id implementation

--- cocktail/persistence/incremental_id.py
+++ cocktail/persistence/incremental_id.py
@@ -10,13 +10,22 @@ from threading import Lock
 from time import sleep
 import transaction
 from cocktail.persistence.datastore import datastore
+from cocktail.persistence.persistentmapping import PersistentMapping
 from ZODB.POSException import ConflictError
 
+ID_CONTAINER_KEY = "id_container"
 RETRY_INTERVAL = 0.1
 
 _lock = Lock()
 
-def incremental_id(key = "incremental_id"):
+@datastore.connection_opened.append
+def create_container(event):
+    root = event.source.root
+    if ID_CONTAINER_KEY not in root:
+        root[ID_CONTAINER_KEY] = PersistentMapping()
+        datastore.commit()
+
+def incremental_id(key = "default"):
     
     _lock.acquire()
 
@@ -28,12 +37,20 @@ def incremental_id(key = "incremental_id"):
             while True:
                 conn.sync()
                 root = conn.root()
-                id = root.get(key, 0) + 1
-                root[key] = id
+                container = root.get(ID_CONTAINER_KEY)
+
+                if container is None:
+                    container = PersistentMapping()
+                    root[ID_CONTAINER_KEY] = container
+                
+                id = container.get(key, 0) + 1
+                container[key] = id
                 try:
                     tm.commit()
                 except ConflictError:
                     sleep(RETRY_INTERVAL)
+                    tm.abort()
+                except:
                     tm.abort()
                 else:
                     return id
