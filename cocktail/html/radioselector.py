ab5d259c60768c27d754ddaf72d380b0425d57fb
Mart? Congost <marti.congost@whads.com>
2009-03-10 13:38:34 +0100

    Improvements to the RadioSelector class (member binding, add <label> tags)

--- cocktail/html/radioselector.py
+++ cocktail/html/radioselector.py
@@ -12,17 +12,32 @@ from cocktail.html.selector import Selector
 
 class RadioSelector(Selector):
 
+    def _ready(self):
+
+        if not self.name and self.data_display:
+            self.name = self.data_display.get_member_name(
+                self.member,
+                self.language
+            )
+
+        Selector._ready(self)
+
     def create_entry(self, value, label, selected):
-        entry = Element()
         
-        input = Element("input")        
-        input["type"] = "radio"
-        input["value"] = value
-        input["checked"] = selected        
-        input["name"] = self.name
+        entry = Element()
+        entry_id = entry.require_id()
+        
+        entry.input = Element("input")
+        entry.input["type"] = "radio"
+        entry.input["value"] = value
+        entry.input["checked"] = selected
+        entry.input["name"] = self.name
         entry.append(input)
         
-        entry.append(label)
+        entry.label = Element("label")
+        entry.label["for"] = entry_id
+        entry.label.append(label)
+        entry.append(entry.label)
 
         return entry
 
