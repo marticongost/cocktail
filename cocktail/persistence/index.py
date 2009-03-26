4cb1d6e28d257f779d6c3d81cfde87b4d2ddd69c
Mart? Congost <marti.congost@whads.com>
2009-03-26 18:42:56 +0100

    Change persistence.Index so that it always supports adding entries to the `None` key, regardless of the type of other keys (for example, this makes it possible to mix None and datetime values on a single index)

--- cocktail/persistence/index.py
+++ cocktail/persistence/index.py
@@ -9,8 +9,9 @@
 from persistent import Persistent
 from cocktail.modeling import empty_set
 
-# Index class
-#------------------------------------------------------------------------------
+infinite = object()
+
+
 class Index(Persistent):
     """A persistent index that supports multiple entries per key. Used to
     maintain an index for a non unique field.
@@ -18,89 +19,227 @@ class Index(Persistent):
 
     def __init__(self, mapping):
         self.__groups = mapping
+        self.__none_entries = set()
 
     def add(self, key, value):
 
-        group = self.__groups.get(key)
+        if key is None:
+            self.__none_entries.add(value)
+            self._p_changed = True
+        else:
+            group = self.__groups.get(key)
 
-        if group is None:
-            self.__groups[key] = group = set()
-     
-        group.add(value)
-        
-        self._p_changed = True
-        self.__groups._p_changed = True
-
-    def remove(self, key, value):
-        group = self.__groups.get(key)
-        
-        if group is not None:
-            group.discard(value)
-
+            if group is None:
+                self.__groups[key] = group = set()
+         
+            group.add(value)
+            
             self._p_changed = True
             self.__groups._p_changed = True
 
+    def remove(self, key, value):
+        
+        if key is None:
+            self.__none_entries.discard(value)
+            self._p_changed = True
+        else:
+
+            group = self.__groups.get(key)
+            
+            if group is not None:
+                group.discard(value)
+
+                self._p_changed = True
+                self.__groups._p_changed = True
+
     def __getitem__(self, key):
         
-        if isinstance(key, slice):
-            
-            if key.step != 1:
-                raise ValueError("Steps other than 1 not accepted")
-
-            return self.values(
-                min = key.start,
-                max = key.end,
-                excludemin = False,
-                excludemax = True)
-
+        if isinstance(key, slice):        
+            raise ValueError(
+                "Slicing an index is not supported; use keys()/values() "
+                "instead")
         else:
-            return self.__groups.get(key, empty_set)
+            if key is None:
+                return self.__none_entries
+            else:
+                return self.__groups.get(key, empty_set)
 
     def __delitem__(self, key):
-        self.__groups.__delitem__(key)
+        if key is None:
+            self.__none_entries = set()
+        else:
+            self.__groups.__delitem__(key)
+            self.__groups._p_changed = True
+
         self._p_changed = True
-        self.__groups._p_changed = True
 
-    def keys(self):
-        return self.__groups.keys()
+    def keys(self,
+        min = infinite,
+        max = infinite,
+        excludemin = False,
+        excludemax = False):
+        
+        return list(self.iterkeys(
+            min = min,
+            max = max,
+            excludemin = excludemin,
+            excludemax = excludemax            
+        ))
 
-    def values(self, *args, **kwargs):        
-        return list(self.itervalues(*args, **kwargs))
+    def values(self,
+        min = infinite,
+        max = infinite,
+        excludemin = False,
+        excludemax = False):
+        
+        return list(self.itervalues(
+            min = min,
+            max = max,
+            excludemin = excludemin,
+            excludemax = excludemax            
+        ))
 
-    def items(self, *args, **kwargs):
-        return list(self.iteritems(*args, **kwargs))
+    def items(self,
+        min = infinite,
+        max = infinite,
+        excludemin = False,
+        excludemax = False):
+        
+        return list(self.iteritems(
+            min = min,
+            max = max,
+            excludemin = excludemin,
+            excludemax = excludemax            
+        ))
 
-    def itervalues(self, *args, **kwargs):
-        for group in self.__groups.itervalues(*args, **kwargs):
-            for item in group:
+    def itervalues(self,
+        min = infinite,
+        max = infinite,
+        excludemin = False,
+        excludemax = False):
+
+        if (min is infinite or (min is None and not excludemin)) \
+        and (max is not None or (max is None and not excludemax)) \
+        and self.__none_entries:
+            for item in self.__none_entries:
                 yield item
+       
+        if max is not None:
 
-    def iterkeys(self):
-        return self.__groups.iterkeys()
+            if min is infinite:
+                min = None
 
-    def iteritems(self, *args, **kwargs):
-        for key, group in self.__groups.iteritems(*args, **kwargs):
-            for item in group:
-                yield key, item
+            if max is infinite:
+                max = None
+            
+            for group in self.__groups.itervalues(
+                min = min,
+                max = max,
+                excludemin = excludemin and min is not None,
+                excludemax = excludemax and max is not None
+            ):
+                for item in group:
+                    yield item
 
-    def minKey(self):
-        return self.__groups.minKey()
+    def iterkeys(self,
+        min = infinite,
+        max = infinite,
+        excludemin = False,
+        excludemax = False):
+        
+        if (min is infinite or (min is None and not excludemin)) \
+        and (max is not None or (max is None and not excludemax)) \
+        and self.__none_entries:
+            yield None
+        
+        if max is not None:
+
+            if min is infinite:
+                min = None
+
+            if max is infinite:
+                max = None
+            
+            for key in self.__groups.iterkeys(
+                min = min,
+                max = max,
+                excludemin = excludemin and min is not None,
+                excludemax = excludemax and max is not None
+            ):
+                yield key
+
+    def iteritems(self,
+        min = infinite,
+        max = infinite,
+        excludemin = False,
+        excludemax = False):
+        
+        if (min is infinite or (min is None and not excludemin)) \
+        and (max is not None or (max is None and not excludemax)) \
+        and self.__none_entries:
+            for item in self.__none_entries:
+                yield None, item
+
+        if max is not None:
+
+            if min is infinite:
+                min = None
+
+            if max is infinite:
+                max = None
+
+            for key, group in self.__groups.iteritems(
+                min = min,
+                max = max,
+                excludemin = excludemin and min is not None,
+                excludemax = excludemax and max is not None
+            ):                
+                for item in group:
+                    yield key, item
+
+    def minKey(self):        
+        if self.__none_entries:
+            return None
+        else:
+            return self.__groups.minKey()
 
     def maxKey(self):
-        return self.__groups.maxKey()
+        try:
+            return self.__groups.maxKey()
+        except ValueError:
+            if self.__none_entries:
+                return None
+            else:
+                raise
 
     def __len__(self):
-        return self.__groups.__len__()
+        count = self.__groups.__len__()
+
+        if self.__none_entries:
+            count += 1
+
+        return count
 
     def __iter__(self):
-        return self.__groups.__iter__()
+        if self.__none_entries:
+            yield None
+
+        for group in self.__groups:
+            yield group
 
     def __contains__(self, key):
-        return self.__groups.__contains__(key)
+        if key is None:
+            return bool(self.__none_entries)
+        else:
+            return self.__groups.__contains__(key)
     
     def __notzero__(self):
-        return self.__groups.__notzero__()
+        return self.__none_entries or self.__groups
 
     def has_key(self, key):
-        return self.__groups.has_key(key)
+        
+        if key is None:
+            return bool(self.__none_entries)
+        else:
+            return self.__groups.has_key(key)
 
