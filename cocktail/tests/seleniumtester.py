582820cb83c5dd331125d10d18b455e4be16965c
Mart? Congost <marti.congost@whads.com>
2009-05-25 11:17:30 +0200

    Several fixes to the selenium-nose integration plugin

--- cocktail/tests/seleniumtester.py
+++ cocktail/tests/seleniumtester.py
@@ -8,18 +8,56 @@ A plugin that integrates Selenium with the Nose testing framework.
 @since:			May 2009
 """
 import os
+from urlparse import urlparse
+from cocktail.modeling import wrap
 from nose.plugins import Plugin
 from selenium import selenium as SeleniumSession
 
+_selenium_site_address = None
 _selenium_sessions = []
+_current_selenium_session = None
+
+def get_selenium_site_address():
+    return _selenium_site_address
 
 def selenium_test(func):
 
     def wrapper(*args):
         for session in _selenium_sessions:
-            yield (func, session) + args
+            yield (_selenium_test_factory(func, session),) + args
 
+    wrap(func, wrapper)
     return wrapper
+
+selenium_test.__test__ = False
+
+def _selenium_test_factory(test_func, session):
+    
+    def wrapper(*args, **kwargs):
+        global _current_selenium_session
+        previous_session = _current_selenium_session
+        _current_selenium_session = session
+        _current_selenium_session.delete_all_visible_cookies()
+        try:
+            return test_func(*args, **kwargs)
+        finally:
+            _current_selenium_session = previous_session
+
+    wrap(test_func, wrapper)
+    wrapper.description = "%s (%s)" % (
+        wrapper.func_name,
+        session.browserStartCommand
+    )
+    return wrapper
+
+
+class SeleniumSessionProxy(object):
+
+    def __getattribute__(self, key):
+        return getattr(_current_selenium_session, key)
+
+
+browser = SeleniumSessionProxy()
 
 
 class SeleniumTester(Plugin):
@@ -49,6 +87,8 @@ class SeleniumTester(Plugin):
     
     def configure(self, options, conf):
 
+        global _selenium_site_address
+
         Plugin.configure(self, options, conf)
 
         if self.enabled \
@@ -57,6 +97,9 @@ class SeleniumTester(Plugin):
         and options.selenium_browsers \
         and options.selenium_url:
 
+            url = urlparse(options.selenium_url)
+            _selenium_site_address = url.hostname, url.port or 80
+            
             for browser_command in options.selenium_browsers.split(","):
                 session = SeleniumSession(
                     options.selenium_host,
@@ -71,7 +114,7 @@ class SeleniumTester(Plugin):
         for session in _selenium_sessions:
             session.start()
 
-    def finalize(self):
+    def finalize(self, result):
         # Close all browsers after all tests have run
         for session in _selenium_sessions:
             session.stop()
