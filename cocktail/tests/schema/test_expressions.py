668b45430c9688939548992180c121574dbd7e64
Jordi Fern?ndez <jordi.fernandez@whads.com>
2009-10-21 13:30:01 +0200

    Implemented the RangeIntersection expression and its tests.

--- cocktail/tests/schema/test_expressions.py
+++ cocktail/tests/schema/test_expressions.py
@@ -47,3 +47,105 @@ class ComparisonTestCase(TestCase):
         assert LowerEqualExpression(None, None).eval({})
         assert LowerEqualExpression(None, 3).eval({})
 
+    def test_eval_between(self):
+        from cocktail.schema.expressions import Expression
+
+        # TODO: solve invalid operand order
+
+        def between(x, i, j, **kwargs):
+            return Expression.wrap(x).between(i, j, **kwargs).eval({})
+
+        # Discrete values
+        assert between(1, 1, 2)
+        assert not between(2, 1, 2)
+        assert between(2, 1, 3)
+        assert not between(1, 1, 2, excludemin = True)
+        assert between(2, 1, 2, excludemax = False)
+
+        # None value
+        assert not between(None, 10, 25)
+        assert not between(None, 10, None)
+        assert between(None, None, 3)
+        assert not between(None, None, 3, excludemin = True)
+
+        # Infinite lower bound
+        assert between(-175, None, 0)
+
+        # Infinite upper bound
+        assert between(175, 0, None)
+
+        # Infinite bounds (lower and upper)
+        assert between(0, None, None)
+        assert between(None, None, None)
+        assert not between(None, None, None, excludemin = True)
+
+    def test_eval_range_intersection(self):
+        
+        # TODO: solve invalid operand order
+
+        from cocktail.schema.expressions import RangeIntersectionExpression
+
+        def intersect(a, b, c, d, **kwargs):
+            return RangeIntersectionExpression(a, b, c, d, **kwargs).eval({})
+        
+        # Completely apart
+        assert not intersect(1, 2, 3, 4)
+        assert not intersect(3, 4, 1, 2)
+        assert not intersect(None, 1, 2, 3)
+        assert not intersect(2, 3, None, 1)
+        assert not intersect(1, 2, 3, None)
+        assert not intersect(3, None, 1, 2)
+        
+        # Single point intersection
+        assert intersect(1, 2, 2, 3)
+        assert not intersect(3, 4, 2, 3)
+        assert not intersect(1, 2, 2, 3, excludemin = True)
+        assert intersect(3, 4, 2, 3, excludemax = False)
+
+        # Contained
+        assert intersect(1, 4, 2, 3)
+        assert intersect(2, 3, 1, 4)
+        assert intersect(None, None, 1, 2)
+        assert intersect(1, 2, None, None)
+        assert intersect(None, 3, 1, 2)
+        assert intersect(1, 2, None, 3)
+        assert intersect(0, None, 1, 2)
+        assert intersect(1, 2, 0, None)
+
+        # Equal
+        assert intersect(1, 2, 1, 2)
+        assert intersect(1, 2, 1, 2, excludemin = True)
+        assert intersect(1, 5, 1, 5, excludemin = True)
+        assert intersect(1, 2, 1, 2, excludemax = True)
+        assert intersect(1, 2, 1, 2, excludemin = True, excludemax = True)
+
+        assert intersect(None, None, None, None)
+        assert intersect(None, None, None, None, excludemin = True)
+        assert intersect(None, None, None, None, excludemax = False)
+        assert intersect(None, None, None, None,
+            excludemin = True,
+            excludemax = False
+        )
+
+        assert intersect(None, 1, None, 1)
+        assert intersect(None, 1, None, 1, excludemin = True)
+        assert intersect(None, 1, None, 1, excludemax = False)
+        assert intersect(None, 1, None, 1,
+            excludemin = True,
+            excludemax = False
+        )
+
+        assert intersect(1, None, 1, None)
+        assert intersect(1, None, 1, None, excludemin = True)
+        assert intersect(1, None, 1, None, excludemax = False)
+        assert intersect(1, None, 1, None,
+            excludemin = True,
+            excludemax = False
+        )
+
+        # Partial overlap
+        assert intersect(1, 3, 2, 4)
+        assert intersect(2, 4, 1, 3)
+        assert intersect(None, 5, 3, None)
+        assert intersect(3, None, None, 5)
+
