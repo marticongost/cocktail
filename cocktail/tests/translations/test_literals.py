df6c52d513a9a1df462c8fd6448121259147371a
Mart? Congost <marti.congost@whads.com>
2009-10-27 12:54:42 +0100

    Reversed the changes of the 68eb65e195d8 changeset to the translation of decimal value; Use the support of the Decimal type for significant places instead (ie. Decimal("15.00")).

--- cocktail/tests/translations/test_literals.py
+++ cocktail/tests/translations/test_literals.py
@@ -16,38 +16,38 @@ class DecimalTranslationTestCase(TestCase):
 
     def test_translate(self):
 
-        ca_es_values = (
-            (Decimal("0"), "0"),
-            (Decimal("-0"), "-0"), # Heh...
-            (Decimal("+0"), "0"), # Heh...
-            (Decimal("1"), "1"),
-            (Decimal("+1"), "1"),
-            (Decimal("105"), "105"),
-            (Decimal("50.0"), "50,0"),
-            (Decimal("150.25"), "150,25"),
-            (Decimal("0.3333"), "0,3333"),
-            (Decimal("5000"), "5.000"),
-            (Decimal("100000"), "100.000"),
-            (Decimal("5.000"), "5,000"),
-            (Decimal("100.000"), "100,000"),
-            (Decimal("15000.03"), "15.000,03"),
-            (Decimal("250000.3"), "250.000,3"),
-            (Decimal("-1"), "-1"),
-            (Decimal("-105"), "-105"),
-            (Decimal("-50.0"), "-50,0"),
-            (Decimal("-150.25"), "-150,25"),
-            (Decimal("-0.3333"), "-0,3333"),
-            (Decimal("-5000"), "-5.000"),
-            (Decimal("-100000"), "-100.000"),
-            (Decimal("-5.000"), "-5,000"),
-            (Decimal("-100.000"), "-100,000"),
-            (Decimal("-15000.03"), "-15.000,03"),
-            (Decimal("-250000.3"), "-250.000,3")
-        )
-
         values = (
-            ("ca", ca_es_values),
-            ("en", (
+            (["ca", "es"],  (
+                (Decimal("0"), "0"),
+                (Decimal("-0"), "-0"), # Heh...
+                (Decimal("+0"), "0"), # Heh...
+                (Decimal("1"), "1"),
+                (Decimal("+1"), "1"),
+                (Decimal("105"), "105"),
+                (Decimal("50.0"), "50,0"),
+                (Decimal("50.00"), "50,00"),
+                (Decimal("150.25"), "150,25"),
+                (Decimal("150.2500"), "150,2500"),
+                (Decimal("0.3333"), "0,3333"),
+                (Decimal("5000"), "5.000"),
+                (Decimal("100000"), "100.000"),
+                (Decimal("5.000"), "5,000"),
+                (Decimal("100.000"), "100,000"),
+                (Decimal("15000.03"), "15.000,03"),
+                (Decimal("250000.3"), "250.000,3"),
+                (Decimal("-1"), "-1"),
+                (Decimal("-105"), "-105"),
+                (Decimal("-50.0"), "-50,0"),
+                (Decimal("-150.25"), "-150,25"),
+                (Decimal("-0.3333"), "-0,3333"),
+                (Decimal("-5000"), "-5.000"),
+                (Decimal("-100000"), "-100.000"),
+                (Decimal("-5.000"), "-5,000"),
+                (Decimal("-100.000"), "-100,000"),
+                (Decimal("-15000.03"), "-15.000,03"),
+                (Decimal("-250000.3"), "-250.000,3")
+            )),
+            (["en"], (
                 (Decimal("0"), "0"),
                 (Decimal("-0"), "-0"), # Again...
                 (Decimal("+0"), "0"),
@@ -55,7 +55,9 @@ class DecimalTranslationTestCase(TestCase):
                 (Decimal("+1"), "1"),
                 (Decimal("105"), "105"),
                 (Decimal("50.0"), "50.0"),
+                (Decimal("50.00"), "50.00"),
                 (Decimal("150.25"), "150.25"),
+                (Decimal("150.2500"), "150.2500"),
                 (Decimal("0.3333"), "0.3333"),
                 (Decimal("5000"), "5,000"),
                 (Decimal("100000"), "100,000"),
@@ -77,8 +79,9 @@ class DecimalTranslationTestCase(TestCase):
             ))
         )
 
-        for language, language_values in values:
-            with language_context(language):
-                for raw, expected in language_values:
-                    self.assertEqual(translations(raw), expected)
+        for languages, tests in values:
+            for language in languages:
+                with language_context(language):
+                    for raw, expected in tests:
+                        assert translations(raw) == expected
 
