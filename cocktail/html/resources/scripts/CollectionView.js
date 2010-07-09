b0b30461c55d9c9b8593acef9dbaccd23c225071
Mart? Congost <marti.congost@whads.com>
2010-07-09 12:25:26 +0200

    Fixed a regression in the client side scripts for CollectionView

--- cocktail/html/resources/scripts/CollectionView.js
+++ cocktail/html/resources/scripts/CollectionView.js
@@ -3,7 +3,7 @@ cocktail.bind(".CollectionView", function ($view) {
     // Row activation
     if (this.activationControl) {
         $view.find(".collection_display").bind("activated", function () {
-            $view.find(collectionView.activationControl).click();
+            $view.find($view.get(0).activationControl).click();
         });
     }
 
