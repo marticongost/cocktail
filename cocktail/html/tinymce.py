b9aac53fc4ba0d109087f44a7a0fe8b34cf35f06
Mart? Congost <marti.congost@whads.com>
2010-10-21 18:06:56 +0200

    Deprecated TinyMCE.default_tinymce_params in favor of TinyMCE.tinymce_params

--- cocktail/html/tinymce.py
+++ cocktail/html/tinymce.py
@@ -6,6 +6,7 @@ u"""
 @organization:	Whads/Accent SL
 @since:			September 2008
 """
+from warnings import warn
 from simplejson import dumps
 from cocktail.translations import get_language
 from cocktail.html import Element, templates
@@ -14,7 +15,14 @@ from cocktail.html.databoundcontrol import DataBoundControl
 
 class TinyMCE(Element, DataBoundControl):
     
-    default_tinymce_params = {}
+    class __metaclass__(Element.__metaclass__):
+        def __init__(cls, name, bases, members):
+            Element.__metaclass__.__init__(cls, name, bases, members)
+            if "tinymce_params" not in members:
+                cls.tinymce_params = {}
+
+    default_tinymce_params = {} # deprecated
+    tinymce_params = {}
 
     def __init__(self, *args, **kwargs):
         Element.__init__(self, *args, **kwargs)
@@ -25,6 +33,27 @@ class TinyMCE(Element, DataBoundControl):
         self.add_resource(
             "/resources/scripts/tinymce/jscripts/tiny_mce/tiny_mce_src.js")
 
+    def aggregate_tinymce_params(self):
+
+        params = {}
+
+        for cls in reversed(self.__class__.__mro__):
+            class_params = getattr(cls, "tinymce_params", None)
+            if class_params:
+                params.update(class_params)
+
+        if self.default_tinymce_params:
+            warn(
+                "TinyMCE.default_tinymce_params is deprecated, use "
+                "TinyMCE.tinymce_params instead",
+                DeprecationWarning,
+                stacklevel = 2
+            )
+            params.update(self.default_tinymce_params)
+
+        params.update(self.tinymce_params)
+        return params
+
     def _build(self):
         self.textarea = templates.new("cocktail.html.TextArea")
         self.append(self.textarea)
@@ -32,11 +61,13 @@ class TinyMCE(Element, DataBoundControl):
         
     def _ready(self):
         Element._ready(self)
-        params = self.default_tinymce_params.copy()
-        params.update(self.tinymce_params)
-        params.setdefault("language", get_language())
+        params = self.aggregate_tinymce_params()
+
+        # Override essential TinyMCE parameters
         params["mode"] = "exact"
+        params["language"] = get_language()
         params["elements"] = self.textarea.require_id()
+
         self.set_client_param("tinymceSettings", params);
     
     def _get_value(self):
