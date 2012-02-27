ff8c86c221fb4aa0b1b0a59a8773e9b55c2bbf0b
Mart? Congost <marti.congost@whads.com>
2012-02-27 10:02:40 +0100

    The persistence.transactional decorator now delegates its behavior to a new persistence.transaction function. Also, a new 'desist' parameter was added to both.

--- cocktail/persistence/transactional.py
+++ cocktail/persistence/transactional.py
@@ -10,37 +10,45 @@ from cocktail.persistence.datastore import datastore
 
 verbose = True
 
-def transactional(max_attempts = 3, before_retrying = None):
+def transactional(*trans_args, **trans_kwargs):
     
-    def decorator(transactional_code):
+    def decorator(action):
     
-        @wraps(transactional_code)
-        def wrapper(*args, **kwargs):
-
-            for i in range(max_attempts):
-                if i > 0:
-                    if verbose:
-                        print styled(
-                            "Retrying transaction %s (%d/%d)" % (
-                                transactional_code,
-                                i,
-                                max_attempts - 1
-                            ), 
-                            {1: "yellow", 2: "brown", 3: "red"}.get(i, "violet")
-                        )
-                    if before_retrying is not None:
-                        before_retrying()
-                try:
-                    rvalue = transactional_code(*args, **kwargs)
-                    datastore.commit()
-                    return rvalue
-                except ConflictError:
-                    datastore.abort()
-                    datastore.sync()
-            
-            raise
+        @wraps(action)
+        def wrapper(*action_args, **action_kwargs):
+            return transaction(
+                lambda: action(*action_args, **action_kwargs), 
+                *trans_args,
+                **trans_kwargs
+            )
 
         return wrapper
     
     return decorator
+
+def transaction(action, max_attempts = 3, before_retrying = None, desist = None):
+
+    for i in range(max_attempts):
+        if i > 0:
+            if verbose:
+                print styled(
+                    "Retrying transaction %s (%d/%d)" % (
+                        action,
+                        i,
+                        max_attempts - 1
+                    ), 
+                    {1: "yellow", 2: "brown", 3: "red"}.get(i, "violet")
+                )
+            if before_retrying is not None:
+                before_retrying()
+        try:
+            rvalue = action(*args, **kwargs)
+            datastore.commit()
+            return rvalue
+        except ConflictError:
+            datastore.abort()
+            datastore.sync()
+            if desist is not None and desist():
+                break
+    raise
 
