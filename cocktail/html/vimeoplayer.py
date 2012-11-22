4239ef9eba9472312b373b5a116ba038882168fe
Mart? Congost <marti.congost@whads.com>
2012-11-22 15:54:36 +0100

    Implemented a client side abstraction layer for video players, providing a uniform API for the YouTube and Vimeo embeded players

--- cocktail/html/vimeoplayer.py
+++ cocktail/html/vimeoplayer.py
@@ -17,7 +17,6 @@ class VimeoPlayer(Element):
     vimeo_autoplay = False
     vimeo_loop = False
     vimeo_api = False
-    vimeo_player_id = None
     vimeo_title = True
     vimeo_byline = True
     vimeo_portrait = True
@@ -27,12 +26,20 @@ class VimeoPlayer(Element):
         self["frameborder"] = "0"
 
     def _ready(self):
+
+        if self.vimeo_api:
+            self.require_id()
+            self.add_class("scriptable_video_player")
+            self.add_resource("/cocktail/scripts/froogaloop2.min.js")
+            self.add_resource("/cocktail/scripts/VimeoPlayer.js")
+
         self["src"] = self.get_video_url()
         self["width"] = self.width
         self["height"] = self.height
         self["allowfullscreen"] = self.allow_fullscreen
         self["mozallowfullscreen"] = self.allow_fullscreen
         self["webkitallowfullscreen"] = self.allow_fullscreen
+
         Element._ready(self)
 
     def get_video_url(self):
@@ -51,8 +58,8 @@ class VimeoPlayer(Element):
             "portrait=%d" % self.vimeo_portrait
         ]
 
-        if self.vimeo_player_id:
-            params.append("player_id=" + self.vimeo_player_id)
+        if self.vimeo_api:
+            params.append("player_id=" + self["id"])
 
         if self.vimeo_color:
             params.append("color=" + self.vimeo_color.lstrip("#"))
