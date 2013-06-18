69b4dff864b2b801de7331ddc43649ec0c78e181
Jordi Fern?ndez <jordi.fernandez@whads.com>
2013-06-18 16:11:57 +0200

    Use an asynchronous API in the IssuViewer control, to ensure that commands are not executed before the viewer's API is ready

--- cocktail/html/resources/scripts/IssuuViewer.js
+++ cocktail/html/resources/scripts/IssuuViewer.js
@@ -7,12 +7,40 @@
 @since:			June 2013
 -----------------------------------------------------------------------------*/
 
+function onIssuuReadersLoaded() {
+    cocktail.issuuAPIReady = true;
+    jQuery(document).trigger("issuuAPIReady");
+}
+
 cocktail.bind(".IssuuViewer", function ($viewer) {
 
-    window.onIssuuReadersLoaded = function() {
-        var viewer = window.IssuuReaders.get($viewer.get(0).configId);
-        viewer.setPageNumber($viewer.get(0).pageNumber);
-    };
+    var api = null;
 
+    function prepareAPI() {
+        api = IssuuReaders.get($viewer.get(0).configId);
+        $viewer.trigger("issuuViewerReady", api);
+    }
+
+    if (cocktail.issuuAPIReady) {
+        prepareAPI();        
+    }
+    else {
+        jQuery(document).one("issuuAPIReady", prepareAPI);
+    }
+
+    this.issuuAPI = function (callback) {        
+        if (api) {
+            callback(api);
+        }
+        else {
+            $viewer.one("issuuViewerReady", function (e, api) { callback.call(this, api); });
+        }
+    }
+
+    if (this.pageNumber) {
+        this.issuuAPI(function (api) {
+            api.setPageNumber(this.pageNumber);
+        });
+    }
 });
 
