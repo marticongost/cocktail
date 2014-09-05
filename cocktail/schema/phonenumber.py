dc9e1d92280d924639bdda685a76783d3a62b810
Mart? Congost <marti.congost@whads.com>
2014-09-05 11:41:31 +0200

    Normalize and validate phone number members.

--- cocktail/schema/phonenumber.py
+++ cocktail/schema/phonenumber.py
@@ -5,7 +5,15 @@ u"""
 """
 from cocktail.schema.schemastrings import String
 
+PHONE_FORMAT = r"^(00)?\+?\d{1,15}$"
+
 
 class PhoneNumber(String):
-    pass
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("format", PHONE_FORMAT)
+        String.__init__(self, *args, **kwargs)
+
+    def normalization(self, value):
+        return value and value.replace(" ", "")
 
