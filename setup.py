25e1daea0b18a093faf5110fcb5412bc2a5f559b
Mart? Congost <marti.congost@whads.com>
2014-09-25 13:29:11 +0200

    Started the 'feni' branch

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cocktail",
-    version = "eblana",
+    version = "feni",
     author = "Whads/Accent SL",
     author_email = "tech@whads.com",
     description = """A tasty mix of python web development utilities.""",
