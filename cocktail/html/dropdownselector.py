ac9ccf4f9cffdf66ec3294c9ef2373ea3fc0fe70
Mart? Congost <marti.congost@whads.com>
2014-12-16 10:07:52 +0100

    Support multiple levels of grouping in selectors.

--- cocktail/html/dropdownselector.py
+++ cocktail/html/dropdownselector.py
@@ -14,10 +14,18 @@ class DropdownSelector(Selector):
 
     tag = "select"
 
-    def create_group(self, group, items):
+    def create_group(self, group):
         container = Element("optgroup")
-        container["label"] = self.get_group_title(group, items)
-        self._create_entries(items, container)
+        container["label"] = self.get_group_title(group)
+        self._create_entries(group.items, container)
+
+        if group.groups:
+            raise ValueError(
+                "Can't display nested groups usign a <select> control; "
+                "The HTML standard forbids nested <optgroup> elements."
+            )
+
+        self._create_nested_groups(group, container)
         return container
 
     def create_entry(self, value, label, selected):
