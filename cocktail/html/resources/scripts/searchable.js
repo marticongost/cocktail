e0aa7822a519ab72bdd715a31128d9caa46b8554
Marti Congost <marti.congost@whads.com>
2015-02-11 15:22:12 +0100

    Searchable controls now give better feedback during search execution.
    * Added a data-cocktail-searchable-status attribute, to signal the start / end
    of the search
    * Trigger a "searching" event before a search is executed
    * Trigger a "searchCompleted" event after a search is over
    * Yield control to the browser just before executing the search, to prevent
    freezing

--- cocktail/html/resources/scripts/searchable.js
+++ cocktail/html/resources/scripts/searchable.js
@@ -12,6 +12,8 @@ cocktail.searchable = function (searchable, params /* = null */) {
     var $searchable = jQuery(searchable);
     searchable = $searchable.get(0);
 
+    $searchable.attr("data-cocktail-searchable-status", "idle");
+
     searchable.getSearchableEntries = function () {
         return $searchable.find(params && params.entriesSelector || ".entry");
     }
@@ -86,62 +88,84 @@ cocktail.searchable = function (searchable, params /* = null */) {
 
     searchable.applySearch = function (query) {
 
-        var $entries = searchable.getSearchableEntries();
-        var $matches = jQuery();
+        $searchable.attr("data-cocktail-searchable-status", "searching");
 
-        if (!query) {
-            searchable.applyMatchState($entries, true);
-            $matches = $entries;
-            if (highlighted) {
-                $entries.each(function () {
-                    searchable.clearHighlighting(this);
+        $searchable.trigger("searching", {
+            query: query
+        });
+
+        // Pause before executing the search, to give the browser a chance to
+        // display any CSS effect introduced by the change in the
+        // "data-cocktail-searchable-status" attribute or by third party code
+        // triggered by the "searching" event. Without this pause, heavy data
+        // sets can freeze the browser and supress those effects (ie. the
+        // "Searching..." signs wouldn't be displayed).
+        setTimeout(function () {
+
+            var $entries = searchable.getSearchableEntries();
+            var $matches = jQuery();
+
+            if (!query) {
+                searchable.applyMatchState($entries, true);
+                $matches = $entries;
+                if (highlighted) {
+                    $entries.each(function () {
+                        searchable.clearHighlighting(this);
+                    });
+                }
+            }
+            else {
+                // Normalize and tokenize the search query
+                var queryTokens = searchable.tokenizeText(searchable.normalizeText(query));
+
+                // Flag matching / non matching entries
+                $entries.each(function (i) {
+
+                    // Obtain and normalize the searchable text for each entry
+                    var text = this.searchableText;
+                    if (text === undefined) {
+                        text = this.searchableText = searchable.normalizeText(
+                            searchable.getSearchableText(this)
+                        );
+                    }
+
+                    // All tokens must be found for the entry to match
+                    if (searchable.matchTerms(text, queryTokens)) {
+                        searchable.applyMatchState(this, true);
+                        $matches.add(this);
+                        if (highlighted) {
+                            searchable.highlightSearchTerms(this, queryTokens);
+                        }
+                    }
+                    else {
+                        searchable.applyMatchState(this, false);
+                        if (highlighted) {
+                            searchable.clearHighlighting(this);
+                        }
+                    }
                 });
             }
-        }
-        else {
-            // Normalize and tokenize the search query
-            var queryTokens = searchable.tokenizeText(searchable.normalizeText(query));
 
-            // Flag matching / non matching entries
-            $entries.each(function (i) {
+            // Flag matching / non matching entry groups
+            if (params && params.entryGroupsSelector) {
+                var $groups = $searchable.find(params.entryGroupsSelector);
+                $groups.each(function () {
+                    searchable.applyMatchState(this, jQuery(this).find($matches).length);
+                });
+            }
 
-                // Obtain and normalize the searchable text for each entry
-                var text = this.searchableText;
-                if (text === undefined) {
-                    text = this.searchableText = searchable.normalizeText(
-                        searchable.getSearchableText(this)
-                    );
-                }
-
-                // All tokens must be found for the entry to match
-                if (searchable.matchTerms(text, queryTokens)) {
-                    searchable.applyMatchState(this, true);
-                    $matches.add(this);
-                    if (highlighted) {
-                        searchable.highlightSearchTerms(this, queryTokens);
-                    }
-                }
-                else {
-                    searchable.applyMatchState(this, false);
-                    if (highlighted) {
-                        searchable.clearHighlighting(this);
-                    }
-                }
+            $searchable.trigger("searched", {
+                query: query,
+                matches: $matches
             });
-        }
 
-        // Flag matching / non matching entry groups
-        if (params && params.entryGroupsSelector) {
-            var $groups = $searchable.find(params.entryGroupsSelector);
-            $groups.each(function () {
-                searchable.applyMatchState(this, jQuery(this).find($matches).length);
+            $searchable.attr("data-cocktail-searchable-status", "idle");
+
+            $searchable.trigger("searchComplete", {
+                query: query,
+                matches: $matches
             });
-        }
-
-        $searchable.trigger("searched", {
-            query: query,
-            matches: $matches
-        });
+        }, 0);
     }
 
     var matchClass = params && params.matchCSSClass || "match";
