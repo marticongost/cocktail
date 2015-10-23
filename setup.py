455210de80afdad40cabd7736ef5b08d92c522bd
Marti Congost <marti.congost@whads.com>
2015-10-23 12:17:04 +0200

    Started the 'izarra' branch

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cocktail",
-    version = "horilka",
+    version = "izarra",
     author = "Whads/Accent SL",
     author_email = "tech@whads.com",
     description = """A tasty mix of python web development utilities.""",
