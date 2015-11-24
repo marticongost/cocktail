ed4f225d108db5ea98062c8a956ecc7a8cafa603
Marti Congost <marti.congost@whads.com>
2015-11-24 13:34:56 +0100

    Added the ResourceSet.switch_param property.

--- cocktail/html/resources.py
+++ cocktail/html/resources.py
@@ -17,6 +17,7 @@ import urllib2
 from warnings import warn
 import mimetypes
 from pkg_resources import resource_filename
+import cherrypy
 from cocktail.modeling import (
     abstractmethod,
     InstrumentedOrderedSet,
@@ -330,6 +331,7 @@ class ResourceSet(InstrumentedOrderedSet):
         resources = None,
         name = None,
         mime_type = default,
+        switch_param = None,
         **kwargs
     ):
         self.__name = name
@@ -354,6 +356,7 @@ class ResourceSet(InstrumentedOrderedSet):
                 "(multiple choices)"
             )
 
+        self.switch_param = switch_param
         InstrumentedOrderedSet.__init__(self, resources)
 
         for key, value in kwargs.iteritems():
@@ -377,6 +380,11 @@ class ResourceSet(InstrumentedOrderedSet):
         return (
             self._match_mime_type(resource.mime_type)
             and (not resource.set or resource.set == self.__name)
+            and (
+                not self.switch_param
+                or cherrypy.request.params.get(self.switch_param, "on")
+                   != "off"
+            )
         )
 
     @abstractmethod
