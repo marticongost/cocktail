eba0eabc2a8d4e4711c73ade3dece8b31588c03e
Marti Congost <marti.congost@whads.com>
2015-11-24 12:02:23 +0100

    Replace 'preprocessing' with 'filepublication', support source maps for SASS.
    The cocktail.preprocessing API was ill suited to dealing with source maps; for
    one, it assumed that source files should always be transformed into their
    processed form when served, or that they would always be cached in the same way.
    
    The new file_publication API makes far less assumptions, which makes it possible
    to implement different processing and serving patterns for different file types.
    For example, it can create CSS and source map files on demand for SASS files.
    
    As a side effect, the serve_file() method no longer relies on CherryPy's
    implementation, and it now supports serving file-like objects directly.

--- cocktail/controllers/static.py
+++ cocktail/controllers/static.py
@@ -1,80 +1,12 @@
 #-*- coding: utf-8 -*-
-u"""Serve static files.
+u"""Serve and preprocess static files.
 
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
-import os
-from mimetypes import guess_type
-import cherrypy
-from cherrypy.lib.static import serve_file as cp_serve_file
-from cocktail.modeling import DictWrapper
-from cocktail.preprocessing import preprocessors
+from .filepublication import file_publication
 
-def file_publisher(path, content_type = None, disposition = None, name = None):
-    """Creates a CherryPy handler that serves the specified file."""
-
-    @cherrypy.expose
-    def handler(self):
-        return serve_file(path, content_type, disposition, name)
-
-    return handler
-
-class FolderPublisher(object):
-    """Creates a CherryPy handler that serves files in the specified folder."""
-
-    def __init__(self, path):
-        self.path = path
-
-    @cherrypy.expose
-    def __call__(self, *args, **kwargs):
-
-        requested_path = self.path
-
-        for arg in args:
-            requested_path = os.path.join(requested_path, arg)
-
-        # Prevent serving files outside the specified root path
-        if not os.path.normpath(requested_path) \
-        .startswith(os.path.normpath(self.path)):
-            raise cherrypy.HTTPError(403)
-
-        return serve_file(requested_path)
-
-    default = __call__
-
-folder_publisher = FolderPublisher
-
-def serve_file(
-    path,
-    content_type = None,
-    disposition = None,
-    name = None
-):
-    """Serve the indicated file to the current HTTP request."""
-
-    if not disposition:
-        disposition = "inline"
-
-    if disposition not in ("inline", "attachment"):
-        raise ValueError(
-            "disposition must be either 'inline' or 'attachment', not '%s'"
-            % disposition
-        )
-
-    output = preprocessors.preprocess(path)
-
-    if output:
-        mime_type, source = output
-        cherrypy.response.headers["Content-Type"] = mime_type
-        cherrypy.response.headers["Content-Disposition"] = \
-            "%s; filename=%s" % (disposition, name)
-        return source
-
-    return cp_serve_file(path, content_type, disposition, name)
-
-
-class CantHandleFile(Exception):
-    """An exception raised by a MIME type file handler to indicate it cedes
-    control back to the default static file publisher.
-    """
+# For backwards compatibility
+serve_file = file_publication.serve_file
+file_publisher = file_publication.file_publisher
+folder_publisher = file_publication.folder_publisher
 
