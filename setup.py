7ea43dc3acd7cb4aa4afd902e648ec900809d47d
Marti Congost <marti.congost@whads.com>
2016-02-11 12:53:48 +0100

    New versioning scheme: gin -> 1.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cocktail",
-    version = "gin",
+    version = "1.6",
     author = "Whads/Accent SL",
     author_email = "tech@whads.com",
     description = """A tasty mix of python web development utilities.""",
