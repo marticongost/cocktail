d298d2fb3b11c59040d1d9acb60f689bff8c45c7
Marti Congost <marti.congost@whads.com>
2016-02-11 12:52:16 +0100

    New versioning scheme: damassine -> 1.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cocktail",
-    version = "damassine",
+    version = "1.3",
     author = "Whads/Accent SL",
     author_email = "tech@whads.com",
     description = """A tasty mix of python web development utilities.""",
