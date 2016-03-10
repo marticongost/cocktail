e6d4ca3d3710d79cbeb3b9608695edaa4055f1d7
Marti Congost <marti.congost@whads.com>
2016-03-10 11:43:51 +0100

    Support Counter objects on MSExcelExporter

--- cocktail/schema/io.py
+++ cocktail/schema/io.py
@@ -10,6 +10,7 @@ Provides a method to export collections to different mime_types.
 import os
 import pyExcelerator
 from decimal import Decimal
+from collections import Counter
 from cocktail.modeling import ListWrapper, SetWrapper, DictWrapper
 from cocktail.translations import translations, get_language
 from cocktail.typemapping import TypeMapping
@@ -168,6 +169,12 @@ class MSExcelExporter(object):
                 for k, v in value.iteritems()
             )
 
+        def multiline_count(value):
+            return u"\n".join(
+                u"%s: %s" % (k, v)
+                for k, v in value.most_common()
+            )
+
         self.type_exporters = TypeMapping((
             (object, lambda value: unicode(value)),
             (str, lambda value: value.encode("utf-8")),
@@ -182,6 +189,7 @@ class MSExcelExporter(object):
             (set, multiline),
             (ListWrapper, multiline),
             (SetWrapper, multiline),
+            (Counter, multiline_count),
             (dict, multiline_mapping),
             (DictWrapper, multiline_mapping)
         ))
