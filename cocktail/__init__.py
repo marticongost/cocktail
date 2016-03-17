93a1492d9b4c803bfcb39590c5568c3512e7bd73
Marti Congost <marti.congost@whads.com>
2016-03-17 12:55:06 +0100

    New version: 1.8.3

--- cocktail/__init__.py
+++ cocktail/__init__.py
@@ -4,5 +4,5 @@ u"""
 emphasis in declarative programming.
 """
 
-__version__ = "1.8.2"
+__version__ = "1.8.3"
 
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cocktail",
-    version = "1.8.2",
+    version = "1.8.3",
     author = "Whads/Accent SL",
     author_email = "tech@whads.com",
     description = """A tasty mix of python web development utilities.""",
