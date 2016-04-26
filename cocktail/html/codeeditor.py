e512f62d4affc92e5762af6b92daeaea4c925bf3
Marti Congost <marti.congost@whads.com>
2016-04-26 16:13:51 +0200

    Changes to the in browser code editor control.
    - Updated CodeMirror to version 5.14.2.
    - Fixed the resource URIs for cocktail.html.CodeEditor, which had been broken
    by the komovica branch.
    - Renamed cocktail.html.CodeEditor.base_settings to default_settings, to state
    the intended usage of the property more clearly
    - Removed the standard per-syntax settings; per syntax customization is still
    available, but the new mode system in CodeMirror makes having defaults
    unnecessary

--- cocktail/html/codeeditor.py
+++ cocktail/html/codeeditor.py
@@ -13,88 +13,10 @@ from cocktail.html.textarea import TextArea
 
 class CodeEditor(TextArea):
 
-    resources_uri = "cocktail://codemirror/"
-
-    base_settings = {
-        "path": resources_uri + "js/",
-        "indentUnit": 4,
-        "width": "",
-        "height": ""
-    }
-
+    default_settings = {}
+    syntax_settings = {}
     editor_settings = {}
-
-    syntax_settings = {
-        "javascript": {
-            "parserfile": [
-                "tokenizejavascript.js",
-                "parsejavascript.js"
-            ],
-            "stylesheet": resources_uri + "css/jscolors.css",
-            "autoMatchParens": True
-        },
-        "html": {
-            "parserfile": [
-                "parsexml.js",
-                "parsecss.js",
-                "tokenizejavascript.js",
-                "parsejavascript.js",
-                "parsehtmlmixed.js"
-            ],
-            "stylesheet": [
-                resources_uri + "css/xmlcolors.css",
-                resources_uri + "css/jscolors.css",
-                resources_uri + "css/csscolors.css"
-            ]
-        },
-        "xml": {
-            "parserfile": "parsexml.js",
-            "stylesheet": resources_uri + "css/xmlcolors.css",
-            "useHTMLKludges": False
-        },
-        "css": {
-            "parserfile": "parsecss.js",
-            "stylesheet": resources_uri + "css/csscolors.css"
-        },
-        "sparql": {
-            "parserfile": "parsesparql.js",
-            "stylesheet": resources_uri + "css/sparqlcolors.css"
-        },
-        "php": {
-            "parserfile": [
-                "parsexml.js",
-                "parsecss.js",
-                "tokenizejavascript.js",
-                "parsejavascript.js",
-                "../contrib/php/js/tokenizephp.js",
-                "../contrib/php/js/parsephp.js",
-                "../contrib/php/js/parsephphtmlmixed.js"
-            ],
-            "stylesheet": [
-                resources_uri + "css/xmlcolors.css",
-                resources_uri + "css/jscolors.css",
-                resources_uri + "css/csscolors.css",
-                resources_uri + "css/phpcolors.css"
-            ]
-        },
-        "python": {
-            "parserfile": "../contrib/python/js/parsepython.js",
-            "stylesheet": resources_uri + "css/pythoncolors.css",
-            "parserConfig": {"pythonVersion": 2, "strictErrors": True}
-        },
-        "sql": {
-            "parserfile": "../contrib/sql/js/parsesql.js",
-            "stylesheet": resources_uri + "css/sqlcolors.css"
-        },
-        "lua": {
-            "parserfile": "../contrib/lua/js/parselua.js",
-            "stylesheet": resources_uri + "css/luacolors.css"
-        },
-        "ruby": {
-            "parserfile": "../contrib/ruby/js/parseruby.js",
-            "stylesheet": resources_uri + "css/rubycolors.css"
-        }
-    }
+    syntax = None
 
     def __init__(self, *args, **kwargs):
         TextArea.__init__(self, *args, **kwargs)
@@ -104,31 +26,24 @@ class CodeEditor(TextArea):
 
         TextArea._ready(self)
 
-        self.add_resource(self.resources_uri + "js/codemirror.js")
-
-        settings = self.base_settings.copy()
-
-        if self._syntax:
-            settings.update(self.syntax_settings.get(self._syntax))
-
-        settings.update(self.editor_settings)
-
-        self.add_client_code("CodeMirror.fromTextArea('%s', %s)" % (
-            self.require_id(),
-            dumps(settings)
+        self.add_resource("cocktail://codemirror/lib/codemirror.css")
+        self.add_resource("cocktail://codemirror/lib/codemirror.js")
+        self.add_resource("cocktail://codemirror/mode/%s/%s.js" % (
+            self.syntax,
+            self.syntax
         ))
 
-    _syntax = None
+        settings = self.default_settings.copy()
 
-    def _get_syntax(self):
-        return self._syntax
+        syntax_settings = self.syntax_settings.get(self.syntax)
+        if syntax_settings:
+            settings.update(syntax_settings)
 
-    def _set_syntax(self, syntax):
+        settings.update(self.editor_settings)
+        settings["mode"] = self.syntax
 
-        if syntax not in self.syntax_settings:
-            raise ValueError("Unknown syntax: %s" % syntax)
-
-        self._syntax = syntax
-
-    syntax = property(_get_syntax, _set_syntax)
+        self.add_client_code(
+            "CodeMirror.fromTextArea(this, %s)"
+            % dumps(settings)
+        )
 
