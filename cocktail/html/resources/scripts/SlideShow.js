8c1036605e68a58f78a7bcd3803f6094095da631
Marti Congost <marti.congost@whads.com>
2016-04-26 16:15:25 +0200

    Make SlideShow set the right/bottom CSS properties of its fading slides.
    This helps with slides that don't define an explicit width.

--- cocktail/html/resources/scripts/SlideShow.js
+++ cocktail/html/resources/scripts/SlideShow.js
@@ -205,7 +205,9 @@ cocktail.bind(".SlideShow", function ($slideShow) {
             .css({
                 "position": "absolute",
                 "top": 0,
-                "left": 0
+                "left": 0,
+                "right": 0,
+                "bottom": 0
             });
 
         cocktail.slideShowTransitions[this.transitionEffect].hide(
