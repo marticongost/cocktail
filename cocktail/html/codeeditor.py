f6c194d767d3dbd8aeeafd936769964567afcc42
Marti Congost <marti.congost@whads.com>
2016-04-27 09:32:31 +0200

    Add support for CodeMirror submodes in cocktail.html.CodeEditor

--- cocktail/html/codeeditor.py
+++ cocktail/html/codeeditor.py
@@ -17,6 +17,9 @@ class CodeEditor(TextArea):
     syntax_settings = {}
     editor_settings = {}
     syntax = None
+    submodes = {
+        "scss": ("css", "text/x-scss")
+    }
 
     def __init__(self, *args, **kwargs):
         TextArea.__init__(self, *args, **kwargs)
@@ -28,20 +31,27 @@ class CodeEditor(TextArea):
 
         self.add_resource("cocktail://codemirror/lib/codemirror.css")
         self.add_resource("cocktail://codemirror/lib/codemirror.js")
-        self.add_resource("cocktail://codemirror/mode/%s/%s.js" % (
-            self.syntax,
-            self.syntax
-        ))
 
         settings = self.default_settings.copy()
+        submode_info = self.submodes.get(self.syntax)
+
+        if submode_info:
+            main_mode, submode = submode_info
+        else:
+            main_mode = self.syntax
+            submode = self.syntax
+
+        self.add_resource(
+            "cocktail://codemirror/mode/%s/%s.js"
+            % (main_mode, main_mode)
+        )
 
         syntax_settings = self.syntax_settings.get(self.syntax)
         if syntax_settings:
             settings.update(syntax_settings)
 
         settings.update(self.editor_settings)
-        settings["mode"] = self.syntax
-
+        settings.setdefault("mode", submode)
         self.add_client_code(
             "CodeMirror.fromTextArea(this, %s)"
             % dumps(settings)
