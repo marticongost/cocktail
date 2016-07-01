ee70f608be0f5bb7f51296add9f979cb48674ecd
Marti Congost <marti.congost@whads.com>
2016-07-01 13:54:17 +0200

    New API for URL objects.
    The new cocktail.urls module introduces object oriented APIs to deal with URLs.
    The new URL class deprecates the old Location class, with the following
    advantages:
    
    - URL is a subclass of unicode, so it is inmutable and hashable
    - Being a subclass of unicode makes it possible to pass it to urllib.urlopen()
      and other third party functions
    - Programmatic, incremental generation of URLs is still possible, using the new
      URLBuilder class
    - Transparent encoding support, including punycode in domains and percent
      encoding of paths and query strings
    - More methods to join, extend and merge full URLs or its parts
    
    Location offered features beyond URL parsing and manipulation; those are still
    available, but have been moved to specific modules in the cocktail.controllers
    package.
    
    - Redirection (the old Location.go() method) is achieved using the functions in
      cocktail.controllers.redirection, or by passing an URL instance to
      cherrypy.HTTPRedirection.
    - Obtaining an URL object for the active request is achieved using the functions
      in cocktail.controllers.request.

---
+++ cocktail/controllers/request.py
@@ -0,0 +1,58 @@
+#-*- coding: utf-8 -*-
+u"""
+
+.. moduleauthor:: Martí Congost <marti.congost@whads.com>
+"""
+import cherrypy
+from cocktail.urls import URLBuilder
+
+_default_ports = {"http": 80, "https": 443}
+
+def get_request_root_url():
+    try:
+        return cherrypy.request._cocktail_request_root_url
+    except AttributeError:
+        url = get_request_root_url_builder().get_url()
+        cherrypy.request._cocktail_request_root_url = url
+        return url
+
+def get_request_url():
+    try:
+        return cherrypy.request._cocktail_request_url
+    except AttributeError:
+        url = get_request_url_builder().get_url()
+        cherrypy.request._cocktail_request_url = url
+        return url
+
+def get_request_root_url_builder():
+
+    url_builder = URLBuilder()
+    headers = cherrypy.request.headers
+    base = cherrypy.request.base
+
+    if base:
+        scheme, rest = base.split("://")
+        url_builder.scheme = headers.get('X-Forwarded-Scheme') or scheme
+        pos = rest.find(":")
+        if pos == -1:
+            url_builder.hostname = rest
+        else:
+            url_builder.hostname = rest[:pos]
+            url_builder.port = rest[pos+1:]
+
+    elif cherrypy.server.socket_host:
+        url_builder.scheme = headers.get('X-Forwarded-Scheme', 'http')
+        url_builder.hostname = cherrypy.server.socket_host
+        port = cherrypy.server.socket_port
+        if port:
+            if port != _default_ports.get(url_builder.scheme):
+                url_builder.port = port
+
+    return url_builder
+
+def get_request_url_builder():
+    url_builder = get_request_root_url_builder()
+    url_builder.path = cherrypy.request.path_info
+    url_builder.query = cherrypy.request.query_string
+    return url_builder
+
