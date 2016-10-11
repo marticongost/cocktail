fddc2364422baee4cc4500ab7ee8be0e3569008b
Marti Congost <marti.congost@whads.com>
2016-10-11 09:46:06 +0200

    Make translations.directionality.get return the default if given an empty locale

--- cocktail/translations/directionality.py
+++ cocktail/translations/directionality.py
@@ -14,11 +14,17 @@ defaults = {
 }
 
 def get(locale):
+
+    if not locale:
+        return get("*")
+
     locale = locale.split("-", 1)[0]
     mapping = getattr(_thread_data, "directionality", None)
     direction = None if mapping is None else mapping.get(locale)
+
     if direction is None:
         direction = defaults.get(locale)
+
     return direction or get("*")
 
 def set(locale, direction):
