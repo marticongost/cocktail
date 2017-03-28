8818546c29acd1e5ea60ae584567fbc74ecd0a3f
Marti Congost <marti.congost@whads.com>
2017-03-28 15:27:32 +0200

    Split cocktail.ui://scripts/ui.js into different modules

---
+++ cocktail/ui/resources/scripts/databinding.js
@@ -0,0 +1,68 @@
+/*-----------------------------------------------------------------------------
+
+
+@author:        Martí Congost
+@contact:       marti.congost@whads.com
+@organization:  Whads/Accent SL
+@since:         March 2017
+-----------------------------------------------------------------------------*/
+
+cocktail.ui.DISPLAY = Symbol("cocktail.ui.DISPLAY");
+
+cocktail.ui.dataBound = function (registration) {
+    registration.properties["dataSource"] = {};
+    registration.properties["dataState"] = {reflected: true};
+}
+
+cocktail.ui.DataSource = class DataSource {
+
+    constructor(parameters = null) {
+    }
+
+    load(parameters = null) {}
+
+    getValue(object, member, language = null) {
+        let value = object[member.name];
+        if (member.translated && language) {
+            return value ? value[language] : null;
+        }
+        else {
+            return value;
+        }
+    }
+}
+
+cocktail.ui.HTTPDataSource = class HTTPDataSource extends cocktail.ui.DataSource {
+
+    constructor(parameters = null) {
+        super(parameters);
+        if (parameters) {
+            this.url = parameters.url;
+        }
+    }
+
+    getRequestParameters(parameters = null) {
+        if (!parameters) {
+            parameters = {};
+        }
+        parameters.url = this.url;
+        parameters.responseType = "json";
+        return parameters;
+    }
+
+    load(parameters = null) {
+
+        parameters = this.getRequestParameters(parameters);
+
+        return new Promise(function (resolve, reject) {
+            cocktail.ui.request(parameters)
+                .then(function (request) {
+                    resolve(request.response);
+                })
+                .catch(function (e) {
+                    reject(e);
+                });
+        });
+    }
+}
+
