dbc6d542720f0a877e267438cb47c351b087b3e0
Marti Congost <marti.congost@whads.com>
2017-05-02 12:37:01 +0200

    Added the cocktail.collections frontend package

---
+++ cocktail/html/resources/scripts/collections.js
@@ -0,0 +1,121 @@
+/*-----------------------------------------------------------------------------
+
+
+@author:        Martí Congost
+@contact:       marti.congost@whads.com
+@organization:  Whads/Accent SL
+@since:         May 2017
+-----------------------------------------------------------------------------*/
+
+cocktail.declare("cocktail.arrays");
+
+cocktail.arrays.normalize = function (collection) {
+    return collection instanceof Array ? collection : Array.from(collection);
+}
+
+cocktail.declare("cocktail.sets");
+
+cocktail.sets.normalize = function (collection) {
+    return collection instanceof Set ? collection : new Set(collection);
+}
+
+cocktail.sets.equal = function (a, b) {
+
+    a = cocktail.sets.normalize(a);
+    b = cocktail.sets.normalize(b);
+
+    if (a.size != b.size) {
+        return false;
+    }
+
+    for (let item of a) {
+        if (!b.has(item)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+cocktail.sets.isSubset = function (a, b) {
+
+    a = cocktail.sets.normalize(a);
+    b = cocktail.sets.normalize(b);
+
+    for (let item of a) {
+        if (!b.has(item)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+cocktail.sets.isSuperset = function (a, b) {
+
+    a = cocktail.sets.normalize(a);
+    b = cocktail.sets.normalize(b);
+
+    for (let item of b) {
+        if (!a.has(item)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+cocktail.sets.addedRemoved = function (oldSet, newSet) {
+    oldSet = cocktail.sets.normalize(oldSet);
+    newSet = cocktail.sets.normalize(newSet);
+    return [
+        cocktail.sets.difference(newSet, oldSet),
+        cocktail.sets.difference(oldSet, newSet)
+    ];
+}
+
+cocktail.sets.update = function (set, items) {
+    for (let item of items) {
+        set.add(item);
+    }
+}
+
+cocktail.sets.intersectionUpdate = function (set, items) {
+    items = cocktail.sets.normalize(items);
+    for (let item of set) {
+        if (!items.has(item)) {
+            set.delete(item);
+        }
+    }
+}
+
+cocktail.sets.differenceUpdate = function (set, items) {
+    for (let item of items) {
+        set.delete(item);
+    }
+}
+
+cocktail.sets.union = function (a, b) {
+    let union = new Set(a);
+    for (let item of b) {
+        union.add(item);
+    }
+    return union;
+}
+
+cocktail.sets.intersection = function (a, b) {
+    let intersection = new Set();
+    for (let item of a) {
+        if (b.has(item)) {
+            intersection.add(item);
+        }
+    }
+    return intersection;
+}
+
+cocktail.sets.difference = function (a, b) {
+    a = cocktail.arrays.normalize(a);
+    b = cocktail.sets.normalize(b);
+    return new Set(a.filter((item) => !b.has(item)));
+}
+
