4d19ececcf780b5bca931cc425ded7331d6148db
Marti Congost <marti.congost@whads.com>
2017-05-16 13:06:03 +0200

    cocktail.ui: added the 'delay' module

---
+++ cocktail/ui/resources/scripts/delay.js
@@ -0,0 +1,45 @@
+/*-----------------------------------------------------------------------------
+
+
+@author:        Martí Congost
+@contact:       marti.congost@whads.com
+@organization:  Whads/Accent SL
+@since:         May 2017
+-----------------------------------------------------------------------------*/
+
+{
+    const TIMER = Symbol();
+
+    cocktail.ui.Delay = class Delayable {
+
+        constructor(threshold, callback, cancelCallback) {
+            this[TIMER] = null;
+            this.threshold = threshold;
+            this.callback = callback;
+            this.cancelCallback = cancelCallback;
+        }
+
+        begin() {
+            this.cancel();
+            this[TIMER] = setTimeout(() => {
+                this[TIMER] = null;
+                this.callback()
+            }, this.threshold);
+        }
+
+        cancel() {
+            if (this[TIMER]) {
+                clearTimeout(this[TIMER]);
+                this[TIMER] = null;
+                if (this.cancelCallback) {
+                    this.cancelCallback();
+                }
+            }
+        }
+
+        get running() {
+            return this[TIMER] !== null;
+        }
+    }
+}
+
