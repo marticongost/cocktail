64f57b70f6b50cd457b388ee9fff184787f8ae61
Marti Congost <marti.congost@whads.com>
2017-09-27 11:23:36 +0200

    The deprecation of the document.body.scrollTop property had broken the infinite scroll script

--- cocktail/html/resources/scripts/infinitescroll.js
+++ cocktail/html/resources/scripts/infinitescroll.js
@@ -50,9 +50,15 @@ cocktail.infiniteScroll = function (params) {
     if (scrollable == document.body) {
         $scrollEventEmitter = jQuery(window);
         if (direction == "portrait") {
+            getViewportOffset = function () {
+                return window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+            };
             getViewportSize = function () { return window.innerHeight; };
         }
         else if (direction == "landscape") {
+            getViewportOffset = function () {
+                return window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft || 0;
+            };
             getViewportSize = function () { return window.innerWidth; };
         }
     }
