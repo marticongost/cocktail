d4a8266b489a1f26e832fb6452370e6c4753733e
Marti Congost <marti.congost@whads.com>
2018-03-22 09:22:34 +0100

    Fixed the CSRF protection client side script for IE / Edge

--- cocktail/html/resources/scripts/csrfprotection.js
+++ cocktail/html/resources/scripts/csrfprotection.js
@@ -29,29 +29,59 @@ cocktail.csrfprotection.setupRequest = function (xhr) {
 
 // Inject a header with the token into all Ajax requests
 // (only works on requests made through the jQuery API)
-jQuery.ajaxPrefilter(function (options, originalOptions, xhr) {
-    cocktail.csrfprotection.setupRequest(xhr);
-});
+if (window.jQuery) {
+    jQuery.ajaxPrefilter(function (options, originalOptions, xhr) {
+        cocktail.csrfprotection.setupRequest(xhr);
+    });
+}
+
+if (!cocktail.bind) {
+    cocktail._selectors = [];
+    cocktail.bind = function (selector, initializer) {
+        cocktail._selectors.push([selector, initializer]);
+    }
+    window.addEventListener("DOMContentLoaded", function () {
+        for (var s = 0; s < cocktail._selectors.length; s++) {
+            var selector = cocktail._selectors[s][0];
+            var initializer = cocktail._selectors[s][1];
+            var elements = document.querySelectorAll(selector);
+            for (var i = 0; i < elements.length; i++) {
+                initializer.call(elements[i]);
+            }
+        }
+    });
+}
 
 // Inject the token into posted forms. Take care of the formmethod attribute!
-cocktail.bind("[formmethod]", function($element) {
-    $element.click(function(e) {
-        jQuery(this).closest("form").data("cocktail-form-method", this.getAttribute("formmethod"));
+cocktail.bind("[formmethod]", function () {
+    this.addEventListener("click", function (e) {
+        var node = this;
+        while (node) {
+            if (node.tagName == "FORM") {
+                node._cocktailFormMethod = this.getAttribute("formmethod");
+                break;
+            }
+            node = node.parentNode;
+        }
     });
 });
 
-cocktail.bind("form", function($form) {
-    $form.submit( function (e) {
-        var method = $form.data("cocktail-form-method") || this.method;
-        if (method && method.toUpperCase() == "POST") {
-            if (!$form.find("[name='" + cocktail.csrfprotection.field + "']").length) {
-                var hidden = document.createElement("input");
-                hidden.type = "hidden";
-                hidden.name = cocktail.csrfprotection.field;
-                hidden.value = cocktail.csrfprotection.token;
-                $form.append(hidden);
+cocktail.bind("form", function () {
+    this.addEventListener("submit", function (e) {
+        var method = this._cocktailFormMethod || this.method;
+        if (method) {
+            method = method.toUpperCase();
+            if (method == "POST" || method == "PUT" || method == "DELETE") {
+                if (!this.querySelector("[name='" + cocktail.csrfprotection.field + "']")) {
+                    var hidden = document.createElement("input");
+                    hidden.setAttribute("type", "hidden");
+                    hidden.setAttribute("name", cocktail.csrfprotection.field);
+                    hidden.setAttribute("value", cocktail.csrfprotection.token);
+                    this.appendChild(hidden);
+                }
             }
         }
+        this._cocktailFormMethod = null;
     });
 });
 
