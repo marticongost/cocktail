9f1353550e59b48293a44525bdf40eeef8e37ae0
Marti Congost <marti.congost@whads.com>
2019-04-16 09:14:11 +0200

    Fixes and improvements to controllers.displayerror
    - Python3 fixes
    - Changed the CSS to a simpler, more subdued scheme
    - Changed the title of the document depending on the response status
    - Reversed traceback order, show the exception at the top

--- cocktail/controllers/displayerror.py
+++ cocktail/controllers/displayerror.py
@@ -4,14 +4,16 @@
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
 import sys
-import cherrypy
+import http.client
 from traceback import extract_tb
 from cgi import escape
 
-def format_error(error_type, error, traceback):
+import cherrypy
 
+
+def format_error(error):
     lines = []
-    for file, num, func, caller in extract_tb(traceback):
+    for file, num, func, caller in reversed(extract_tb(error.__traceback__)):
         lines.append(
             """
             <li>
@@ -37,18 +39,10 @@ def format_error(error_type, error, traceback):
                 <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
                 <title>%(error_type)s</title>
                 <style type="text/css">
-                    html {
-                        background-color: #442F2F;
-                    }
                     body {
                         font-family: Arial, sans-serif;
-                        background-color: #ebdada;
                         color: #333;
                         padding: 2em;
-                        padding-top: 1.5em;
-                        margin: 1em;
-                        border: 1px solid #a05757;
-                        border-radius: 0.6em;
                     }
                     h1 {
                         font-size: 1.5em;
@@ -76,12 +70,9 @@ def format_error(error_type, error, traceback):
                         font-weight: bold;
                     }
                     #error_desc {
-                        display: inline-block;
-                        border: 2px solid #a05757;
-                        border-radius: 0.3em;
-                        background-color: #ddc7c7;
-                        padding: 0.6em;
-                        box-shadow: 2px 2px 5px rgba(0,0,0,0.2);
+                        background-color: #efe2e2;
+                        padding: 0.8em;
+                        margin-left: -0.8em;
                     }
                     #error_type {
                         color: #572828;
@@ -94,28 +85,33 @@ def format_error(error_type, error, traceback):
                 </style>
             </head>
             <body style="">
-                <h1>500 Internal Server Error</h1>
-                <ul>
-                    %(traceback_lines)s
-                </ul>
+                <h1>%(status_code)s %(status_message)s</h1>
                 <div id="error_desc">
                     <span id="error_type">%(error_type)s:</span>
                     <span id="error_message">%(error_message)s</span>
                 </div>
+                <ul>
+                    %(traceback_lines)s
+                </ul>
             </body>
         </html>
         """ % {
-            "error_type": error_type.__name__,
-            "error_message": escape(error.message),
+            "status_code": cherrypy.response.status,
+            "status_message": (
+                cherrypy.response.body
+                or http.client.responses.get(cherrypy.response.status)
+            ),
+            "error_type": error.__class__.__name__,
+            "error_message": escape(error.args[0]) if error.args else "",
             "traceback_lines": "\n".join(lines)
         }
 
+
 def _escape_error_property(string):
-    if isinstance(string, str):
-        string = string.encode("utf-8")
     return escape(string)
+
 
 def display_error():
     cherrypy.response.status = 500
-    cherrypy.response.body = [format_error(*sys.exc_info())]
+    cherrypy.response.body = [format_error(sys.exc_info()[1])]
 
