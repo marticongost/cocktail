2639c2516f69cbab8b5b8c534fd40af12dd27aa4
Marti Congost <marti.congost@whads.com>
2019-04-23 11:45:43 +0200

    Added a custom JSON encoder in jsonutils
    - Added the ExtensibleJSONEncoder class
    - Added the to_json_value() generic method
    - Implement default serialization for the following types:
      - date
      - time
      - datetime
      - Decimal
      - Fraction
      - ListWrapper (and OrderedSet)
      - SetWrapper
      - DictWrapper

--- cocktail/jsonutils.py
+++ cocktail/jsonutils.py
@@ -2,11 +2,20 @@
 
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
+from cocktail.modeling import (
+    GenericMethod,
+    SetWrapper,
+    ListWrapper,
+    DictWrapper
+)
+from datetime import datetime, date, time
 from typing import (
     Dict,
     List,
     Union
 )
+from decimal import Decimal
+from fractions import Fraction
 from copy import deepcopy
 
 import json
@@ -67,4 +76,51 @@ def merge_json_data(
             merge[key] = overlay_value
 
     return merge
+
+
+@GenericMethod
+def to_json_value(value):
+    raise TypeError(f"Can't serialize {obj} to JSON")
+
+
+@to_json_value.implementation_for(datetime)
+def datetime_to_json_value(value):
+    return value.isoformat()
+
+
+@to_json_value.implementation_for(date)
+def date_to_json_value(value):
+    return value.isoformat()
+
+
+@to_json_value.implementation_for(time)
+def time_to_json_value(value):
+    return value.isoformat()
+
+@to_json_value.implementation_for(Decimal)
+def decimal_to_json_value(value):
+    return float(value)
+
+@to_json_value.implementation_for(Fraction)
+def fraction_to_json_value(value):
+    return str(value)
+
+@to_json_value.implementation_for(set)
+@to_json_value.implementation_for(SetWrapper)
+def set_to_json_value(value):
+    return list(value)
+
+@to_json_value.implementation_for(ListWrapper)
+def list_wrapper_to_json_value(value):
+    return value._items
+
+@to_json_value.implementation_for(DictWrapper)
+def dict_wrapper_to_json_value(value):
+    return value._items
+
+
+class ExtensibleJSONEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        return to_json_value(obj)
 
