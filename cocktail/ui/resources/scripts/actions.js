bf4ffa549dd7a30663a8688948b6dbbb2a1c6e4b
Marti Congost <marti.congost@whads.com>
2019-06-07 11:59:00 +0200

    Overhauled the cocktail.ui.Action API to support grouping actions
    - Added the ActionSet class
    - Allow nesting ActionList objects
    - New APIs for Action and ActionList
    - ActionList is now slot based, to simplify styling

--- cocktail/ui/resources/scripts/actions.js
+++ cocktail/ui/resources/scripts/actions.js
@@ -9,21 +9,157 @@
 
 {
     const ID = Symbol("cocktail.ui.Action.ID");
-    const POSITION = Symbol("cocktail.ui.Action.POSITION");
+    const ENABLED = Symbol("cocktail.ui.Action.ENABLED");
 
-    cocktail.ui.Action = class Action {
+    cocktail.ui.ActionSource = class ActionNode {
 
-        constructor(id, position = null) {
+        constructor(id, parameters = null) {
             this[ID] = id;
-            this[POSITION] = position;
+            this.component = null;
+            if (parameters) {
+                Object.assign(this, parameters);
+            }
         }
 
         get id() {
             return this[ID];
         }
 
-        get iconURL() {
+        createUI(context = null) {
+            const component = this.getComponent(context);
+            const element = component.create();
+            element.actionSource = this;
+            return element;
+        }
+
+        getComponent(context) {
+            let component = this.component || this.constructor.defaultComponent;
+            return component.create ? component : component();
+        }
+
+        static get defaultComponent() {
             return null;
+        }
+
+        static get enabled() {
+            const enabled = this[ENABLED];
+            return enabled === undefined ? true : enabled;
+        }
+
+        static set enabled(value) {
+            this[ENABLED] = value;
+        }
+
+        get enabled() {
+            const enabled = this[ENABLED];
+            return enabled === undefined ? this.constructor.enabled : enabled;
+        }
+
+        set enabled(value) {
+            this[ENABLED] = value;
+        }
+
+        compatibleWith(context) {
+            return this.enabled;
+        }
+    }
+}
+
+{
+    const ACTION_MAP = Symbol();
+    const ACTION_LIST = Symbol();
+
+    cocktail.ui.ActionSet = class ActionSet extends cocktail.ui.ActionSource {
+
+        constructor(id, parameters = null) {
+
+            const entries = parameters.entries;
+            delete parameters.entries;
+            super(id, parameters);
+
+            this[ACTION_MAP] = {};
+            this[ACTION_LIST] = [];
+
+            if (entries) {
+                for (let entry of entries) {
+                    this.add(entry);
+                }
+            }
+        }
+
+        static get defaultComponent() {
+            return cocktail.ui.ActionList.withProperties({buttonStyle: "auto"});
+        }
+
+        getEntry(id) {
+            return this[ACTION_MAP][id];
+        }
+
+        entries(context = null) {
+            if (context) {
+                return Array.from(this[ACTION_LIST].filter((entry) => entry.compatibleWith(context)));
+            }
+            else {
+                return Array.from(this[ACTION_LIST]);
+            }
+        }
+
+        add(entry, options = null) {
+            const order = options && options.order;
+
+            if (this[ACTION_MAP][entry.id]) {
+                throw new cocktail.ui.ActionRegistrationError(`An action with id ${entry.id} already exists on ${this}`);
+            }
+
+            if (order === null || order === undefined) {
+                this[ACTION_LIST].push(entry);
+            }
+            else if (typeof(order) == "string") {
+                try {
+                    let [placement, anchorId] = order.split(" ");
+                }
+                catch (e) {
+                    throw new cocktail.ui.ActionRegistrationError(
+                        `"${order}" is not a valid order string; expected "before ACTION_ID" or "after ACTION_ID"`
+                    );
+                }
+
+                const anchor = actionMap[anchorId];
+                if (!anchor) {
+                    throw new cocktail.ui.ActionRegistrationError(
+                        `Invalid anchor "${anchorId}"; no action with that ID exists on ${this}`
+                    );
+                }
+
+                let anchorIndex = actionList.indexOf(anchor);
+                if (placement == "after") {
+                    anchorIndex++;
+                }
+                else if (placement != "before") {
+                    throw new cocktail.ui.ActionRegistrationError(
+                        `Invalid placement "${placement}"; expected "after" or "before"`
+                    );
+                }
+
+                this[ACTION_LIST].splice(anchorIndex, 0, entry);
+            }
+            else {
+                throw new cocktail.ui.ActionRegistrationError(
+                    `Invalid order "${order}", expected null or a string`
+                );
+            }
+
+            this[ACTION_MAP][entry.id] = entry;
+            return entry;
+        }
+    }
+}
+
+{
+    cocktail.ui.Action = class Action extends cocktail.ui.ActionSource {
+
+        static get defaultComponent() {
+            return cocktail.ui.ActionListButton;
         }
 
         get min() {
@@ -34,12 +170,8 @@
             return null;
         }
 
-        get position() {
-            return this[POSITION];
-        }
-
-        set position(value) {
-            this[POSITION] = value;
+        getIconURL(context) {
+            return null;
         }
 
         get translationPrefix() {
@@ -47,19 +179,15 @@
         }
 
         get translationKey() {
-            return `${this.translationPrefix}.${this[ID]}`;
+            return `${this.translationPrefix}.${this.id}`;
         }
 
-        translate() {
+        translate(context) {
             return cocktail.ui.translations[this.translationKey];
         }
 
-        get shortcut() {
+        getShortcut(context) {
             return cocktail.ui.translations[this.translationKey + ".shortcut"];
-        }
-
-        matches(actionBar) {
-            return true;
         }
 
         getState(context) {
@@ -79,16 +207,19 @@
             return "visible";
         }
 
-        createEntry() {
-            return this.entryClass.create();
-        }
-
-        get entryClass() {
-            return cocktail.ui.ActionListButton;
-        }
-
         invoke(context) {
         }
+    }
+}
+
+cocktail.ui.ActionRegistrationError = class ActionRegistrationError {
+
+    constructor(message) {
+        this.message = message;
+    }
+
+    toString() {
+        return this.message;
     }
 }
 
