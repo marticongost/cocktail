79909756118054b3d7e11b6d5f25686fc50444d0
Marti Congost <marti.congost@whads.com>
2019-09-17 11:01:12 +0200

    Custom __repr__ implementation for schema.expressions.CustomExpression

--- cocktail/schema/expressions.py
+++ cocktail/schema/expressions.py
@@ -227,6 +227,9 @@ class CustomExpression(Expression):
     def __init__(self, expression):
         self.expression = expression
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.expression})"
+
     def eval(self, context = None, accessor = None):
         return self.expression(context)
 
