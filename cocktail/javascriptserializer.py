2791ab30b22cd6286518b56ff2aae67f770013b4
Marti Congost <marti.congost@whads.com>
2019-10-15 12:29:11 +0200

    Code cleanup for cocktail.iteration
    - PEP8 compliance
    - Type hinting
    - Added the 'javascript' type
    - Removed an unused list instance in dumps()

--- cocktail/javascriptserializer.py
+++ cocktail/javascriptserializer.py
@@ -1,28 +1,30 @@
-#-*- coding: utf-8 -*-
 """
 
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
+from typing import Any, Union
 from collections import Mapping, Sequence, Set
 
 from . import jsonutils
 
 
 class JS(str):
-    pass
+    """A literal snippet of Javascript, to be included as is when calling
+    `dumps` to generate Javascript output.
+    """
 
 
-def dumps(value):
+javascript = Union[jsonutils.json_data, JS]
 
-    chunks = []
+
+def dumps(value: javascript) -> str:
 
     if isinstance(value, JS):
         return str(value)
 
     if isinstance(value, Mapping):
         return "{%s}" % ",".join(
-            "%s: %s" % (dumps(k), dumps(v))
-            for k, v in value.items()
+            f"{dumps(k)}: {dumps(v)}" for k, v in value.items()
         )
 
     if isinstance(value, (Sequence, Set)) and not isinstance(value, str):
